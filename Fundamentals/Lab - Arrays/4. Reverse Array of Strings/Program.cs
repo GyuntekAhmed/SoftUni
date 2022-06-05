ea6fb6969e68360d459e5bb581c31d4f8d5a6872
Gyuntek <gyuntekahmed@abv.bg>
2022-06-06 02:05:02 +0300

    Added more C.A.

---
+++ Fundamentals/Lab - Arrays/4. Reverse Array of Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Array.Reverse(elements);
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
---
+++ Fundamentals/Lab - Arrays/5. Sum Even Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
