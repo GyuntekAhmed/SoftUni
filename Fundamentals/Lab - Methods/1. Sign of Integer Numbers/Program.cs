0907134a92432f430ff18957dfe499996332ad94
Gyuntek <gyuntekahmed@abv.bg>
2022-06-09 23:39:44 +0300

    Added folder Lab - Methods.sln

---
+++ Fundamentals/Lab - Methods/1. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintSign(number);
+        }
+        static void PrintSign(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero. ");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative. ");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is positive. ");
+
+            }
+
+        }
+    }
+
+}
---
+++ Fundamentals/Lab - Methods/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            numbers = Math.Abs(numbers);
+
+            int sumOfEven = GetSumOfEven(numbers);
+            int sumOfOdd = GetSumOfOddDigits(numbers);
+            int result = GetMultipleOfEvenAndOdds(sumOfEven, sumOfOdd);
+
+            Console.WriteLine(result);
+        }
+
+        static int GetSumOfEven(int number)
+        {
+            int sum = 0;
+            int digits = number;
+
+            while (digits > 0)
+            {
+                int currentDigit = digits % 10;
+
+                if (currentDigit % 2 == 0)
+                {
+                    sum += currentDigit;
+                }
+
+                digits /= 10;
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            int digit = number;
+
+
+            while (digit > 0)
+            {
+                int currentDigit = digit % 10;
+
+                if (currentDigit % 2 != 0)
+                {
+                    sum += currentDigit;
+                }
+
+                digit /= 10;
+            }
+            return sum;
+        }
+
+        static int GetMultipleOfEvenAndOdds(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+    }
+}
