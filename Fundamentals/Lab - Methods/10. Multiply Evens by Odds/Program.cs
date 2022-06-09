0bcf44c57e6eb9832315e9b74ed8c82145f9003b
Gyuntek <gyuntekahmed@abv.bg>
2022-06-09 23:39:44 +0300

    Added folder Lab - Methods.sln

---
+++ Fundamentals/Lab - Methods/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            number = Math.Abs(number);
+
+            int sumOfEven = GetSumOfEven(number);
+            int sumOfOdd = GetSumOfOddDigits(number);
+            int result = GetMultipleOfEvenAndOdds(sumOfEven, sumOfOdd);
+
+            Console.WriteLine(result);
+        }
+
+        static int GetSumOfEven(int number)
+        {
+            int sum = 0;
+            int digits = number;
+
+            while (digits > 0)
+            {
+                int currentDigit = digits % 10;
+
+                if (currentDigit % 2 == 0)
+                {
+                    sum += currentDigit;
+                }
+
+                digits /= 10;
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            int digit = number;
+
+
+            while (digit > 0)
+            {
+                int currentDigit = digit % 10;
+
+                if (currentDigit % 2 != 0)
+                {
+                    sum += currentDigit;
+                }
+
+                digit /= 10;
+            }
+            return sum;
+        }
+
+        static int GetMultipleOfEvenAndOdds(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+    }
+}
---
+++ Fundamentals/Lab - Methods/3. Calculations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _3._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            switch (comand)
+            {
+                case "add":
+                    AddNum(firstNum, secondNum);
+                    break;
+                case "divide":
+                    DivideNum(firstNum, secondNum);
+                    break;
+                case "multiply":
+                    MultiplyNum(firstNum, secondNum);
+                    break;
+                case "subtract":
+                    SubtractNum(firstNum, secondNum);
+                    break;
+            }
+        }
+
+        static void AddNum(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum + secondNum);
+        }
+
+        static void MultiplyNum(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum * secondNum);
+        }
+
+        static void SubtractNum(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum - secondNum);
+        }
+
+        static void DivideNum(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum / secondNum);
+        }
+    }
+}
