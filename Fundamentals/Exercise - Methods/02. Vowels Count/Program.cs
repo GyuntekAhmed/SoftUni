ded0fe6621e04273774b90ade72ae20600658fc4
Gyuntek <gyuntekahmed@abv.bg>
2022-06-12 01:29:43 +0300

    Initial commit

---
+++ Fundamentals/Exercise - Methods/02. Vowels Count/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+
+            PrintNumberOfVowels(text);
+        }
+
+        static void PrintNumberOfVowels(string text)
+        {
+            int count = 0;
+
+            //Console.WriteLine(text.Count(vowles => "aouei".Contains(vowles)));
+            foreach (char vowle in text)
+            {
+
+                if ("aouei".Contains(vowle))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Fundamentals/Exercise - Methods/1. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(PrintBiggerNum(num1,num2,num3));
+
+        }
+
+        static int PrintBiggerNum(int num1, int num2, int num3)
+        {
+            return Math.Min(num1,(Math.Min(num3, num2)));
+        }
+    }
+}
