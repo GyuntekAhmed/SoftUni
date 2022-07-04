c76bf7d0336ecdcfafba398e73957a3ec0e8b314
Gyuntek <gyuntekahmed@abv.bg>
2022-07-05 00:06:11 +0300

    Initial commit

--- Fundamentals/01. Advertisement Message/05. Teamwork Projects/Program.cs
+++ Fundamentals/01. Advertisement Message/05. Teamwork Projects/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Teamwork_Projects
 {
@@ -6,7 +8,89 @@ namespace _05._Teamwork_Projects
     {
         static void Main(string[] args)
         {
-            
+            int countOfTeams = int.Parse(Console.ReadLine());
+            List<Team> teams = new List<Team>();
+
+            for (int i = 0; i < countOfTeams; i++)
+            {
+                string[] currTeamInfo = Console.ReadLine().Split('-');
+                string creator = currTeamInfo[0];
+                string teamName = currTeamInfo[1];
+
+                if (teams.Any(team => team.Name == teamName))
+                {
+                    Console.WriteLine($"Team {teamName} was already created!");
+                }
+                else if (teams.Any(team => team.Creator == creator))
+                {
+                    Console.WriteLine($"{creator} cannot create another team!");
+                }
+                else
+                {
+                    var team = new Team();
+                    team.Name = teamName;
+                    team.Creator = creator;
+                    team.Members = new List<string>();
+                    teams.Add(team);
+                    Console.WriteLine($"Team {teamName} has been created by {creator}!");
+                }
+            }
+
+            string line = Console.ReadLine();
+
+            while (line != "end of assignment")
+            {
+                string[] memberInfo = line.Split(new string[] {"->"}, StringSplitOptions.RemoveEmptyEntries);
+                string memberName = memberInfo[0];
+                string teamToJoin = memberInfo[1];
+
+                if (teams.Any(team => team.Members.Contains(memberName)) || teams.Any(creator => creator.Creator == memberName))
+                {
+                    Console.WriteLine($"Member {memberName} cannot join team {teamToJoin}!");
+                }
+                else if (!teams.Any(team => team.Name == teamToJoin))
+                {
+                    Console.WriteLine($"Team {teamToJoin} does not exist!");
+                }
+                else
+                {
+                    var currentTeam = teams.Find(team => team.Name == teamToJoin);
+                    currentTeam.Members.Add(memberName);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            var completedTeams = teams.Where(x => x.Members.Count > 0);
+            var disbanedTeams = teams.Where(team => team.Members.Count == 0);
+
+            foreach (var team in completedTeams.OrderByDescending(x => x.Members.Count).ThenBy(y => y.Name))
+            {
+                Console.WriteLine($"{team.Name}");
+                Console.WriteLine($"- {team.Creator}");
+
+                foreach (var member in team.Members.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {member}");
+                }
+            }
+
+            Console.WriteLine("Teams to disband:");
+
+            if (disbanedTeams != null)
+            {
+                foreach (var disbanedTeam in disbanedTeams.OrderBy(x => x.Name))
+                {
+                    Console.WriteLine($"{disbanedTeam.Name}");
+                }
+            }
         }
+    }
+
+    class Team
+    {
+        public string Name { get; set; }
+        public string Creator { get; set; }
+        public List<string> Members  { get; set; }
     }
 }
---
+++ Fundamentals/01. Advertisement Message/06. Vehicle Catalogue/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06._Vehicle_Catalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                VehicleType vehicleType;
+                bool isVehicleTypeSuccesfull = Enum.TryParse(input[0], true, out vehicleType);
+
+                if (isVehicleTypeSuccesfull)
+                {
+                    string vehicleModel = input[1];
+                    string vehicleColor = input[2];
+                    int vehicleHorsePower = int.Parse(input[3]);
+
+                    Vehicle currentVehicle = new Vehicle(vehicleType, vehicleModel, vehicleColor, vehicleHorsePower);
+                    vehicles.Add(currentVehicle);
+                }
+            }
+
+            while (true)
+            {
+                string comands = Console.ReadLine();
+
+                if (comands == "Close the Catalogue")
+                {
+                    break;
+                }
+
+                Vehicle desiredVehicle = vehicles.FirstOrDefault(vehicle => vehicle.Model == comands);
+
+                Console.WriteLine(desiredVehicle);
+            }
+
+            List<Vehicle> cars = vehicles.Where(vehicle => vehicle.Type == VehicleType.Car).ToList();
+            List<Vehicle> trucks = vehicles.Where(vehicle => vehicle.Type == VehicleType.Truck).ToList();
+
+            double carsAvarageHorsePower = cars.Count > 0 ? cars.Average(car => car.HorsePower) : 0.00;
+            double trucksAvarageHorsePower = trucks.Count > 0 ? trucks.Average(truck => truck.HorsePower) : 0.00;
+
+            Console.WriteLine($"Cars have average horsepower of: {carsAvarageHorsePower:f2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {trucksAvarageHorsePower:f2}.");
+        }
+    }
+
+    enum VehicleType
+    {
+        Car,
+        Truck
+    }
+    class Vehicle
+    {
+        public Vehicle(VehicleType type, string model, string color, int horsePower)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            HorsePower = horsePower;
+
+        }
+        public VehicleType Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Type: {Type}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Color: {Color}");
+            sb.AppendLine($"Horsepower: {HorsePower}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
