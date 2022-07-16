0dca8a1280fa7663c627f91e0bb221b52a1f023a
Gyuntek <gyuntekahmed@abv.bg>
2022-07-16 22:46:02 +0300

    Initial commit

--- Fundamentals/More Exercise - Strings and Text Processing/01. Extract Person Information/Program.cs
+++ Fundamentals/More Exercise - Strings and Text Processing/01. Extract Person Information/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _01._Extract_Person_Information
 {
@@ -6,7 +7,20 @@ namespace _01._Extract_Person_Information
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                int indexOfNameStart = input.IndexOf('@');
+                int indexOfNameEnd = input.IndexOf('|');
+                string name = input.Substring(indexOfNameStart + 1, indexOfNameEnd - indexOfNameStart - 1);
+                int indexOfAgeStart = input.IndexOf('#');
+                int indexOfAgeEnd = input.IndexOf('*');
+                string age = input.Substring(indexOfAgeStart + 1, indexOfAgeEnd - indexOfAgeStart - 1);
+
+                Console.WriteLine($);
+            }
         }
     }
 }
---
+++ Fundamentals/More Exercise - Strings and Text Processing/05. HTML/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titleOfArticle = Console.ReadLine();
+            string contentOfArticle = Console.ReadLine();
+            List<string> comments = new List<string>();
+
+            while (true)
+            {
+                string lines = Console.ReadLine();
+
+                if (lines == "end of comments")
+                {
+                    break;
+                }
+
+                comments.Add(lines);
+            }
+
+            Console.WriteLine("<h1>");
+            Console.WriteLine(titleOfArticle);
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine(contentOfArticle);
+            Console.WriteLine("</article>");
+
+            foreach (var comment in comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine(comment);
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}
