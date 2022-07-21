20378ec0b792d2c9d052c18b0ad6724490b154c2
Gyuntek <gyuntekahmed@abv.bg>
2022-07-21 22:27:50 +0300

    Initial commit

---
+++ Fundamentals/Lab - Regular Expressions/02. Match Phone Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+            string input = Console.ReadLine();
+
+            string[] matches = Regex.Matches(input, patern).Select(x => x.Value).ToArray();
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
