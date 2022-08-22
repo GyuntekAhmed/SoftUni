3a686e975f49da043056baef6ea88de74f494fb7
Gyuntek <gyuntekahmed@abv.bg>
2022-08-22 23:12:48 +0300

    Initial commit

--- C# Advanced/Exercise - Stacks and Queues/01. Basic Stack Operations/Program.cs
+++ C# Advanced/Exercise - Stacks and Queues/01. Basic Stack Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Basic_Stack_Operations
 {
@@ -6,7 +8,46 @@ namespace _01._Basic_Stack_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> stack = new Stack<int>();
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int countOfNumbers = numbers[0];
+
+            int[] numbersToChek = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int x = 0;
+            int chekPosition = numbers[1];
+            int chekNumber = numbers[2];
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                stack.Push(numbersToChek[i]);
+
+                if (i + 1 == chekPosition)
+                {
+                    x = numbersToChek[i];
+                }
+            }
+
+            if (x == chekNumber)
+            {
+                Console.WriteLine("true");
+            }
+            else if (x != chekNumber)
+            {
+                Console.WriteLine(stack.Min());
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
         }
     }
 }
