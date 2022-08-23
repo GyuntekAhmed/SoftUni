76121f95e118e308d2997caf0e3a34b0bc8173be
Gyuntek <gyuntekahmed@abv.bg>
2022-08-23 22:04:41 +0300

    Initial commit

--- C# Advanced/Exercise - Stacks and Queues/03. Maximum and Minimum Element/Program.cs
+++ C# Advanced/Exercise - Stacks and Queues/03. Maximum and Minimum Element/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._Maximum_and_Minimum_Element
 {
@@ -6,7 +8,45 @@ namespace _03._Maximum_and_Minimum_Element
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> stack = new Stack<int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int[] command = Console.ReadLine()
+                                .Split(' ')
+                                .Select(int.Parse)
+                                .ToArray();
+
+                switch (command[0])
+                {
+                    case 1:
+                        stack.Push(command[1]);
+                        break;
+                    case 2:
+                        if (stack.Count > 0)
+                        {
+                            stack.Pop();
+                            break;
+                        }
+                        break;
+                    case 3:
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+                    case 4:
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
         }
     }
 }
---
+++ C# Advanced/Exercise - Stacks and Queues/04. Fast Food/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            Queue<int> ordersQueue = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+
+            Console.WriteLine(ordersQueue.Max());
+
+            int countOfOrders = ordersQueue.Count;
+
+            for (int order = 1; order <= countOfOrders; order++)
+            {
+                if (quantityOfFood >= ordersQueue.Peek())
+                {
+                    quantityOfFood -= ordersQueue.Peek();
+                    ordersQueue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (ordersQueue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: " + string.Join(" ", ordersQueue));
+            }
+        }
+    }
+}
