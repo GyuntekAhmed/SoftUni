f350dfe118389c0355598ef7368168abec95492b
Gyuntek <gyuntekahmed@abv.bg>
2022-08-27 23:45:22 +0300

    Initial commit

---
+++ C# Advanced/Lab - Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
