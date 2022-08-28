ff3cb7d76052215d7b4d21a97134abbf8da6103b
Gyuntek <gyuntekahmed@abv.bg>
2022-08-28 22:28:17 +0300

    Initial commit

--- C# Advanced/Lab - Multidimensional Arrays/3. Primary Diagonal/Program.cs
+++ C# Advanced/Lab - Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -7,7 +7,21 @@ namespace _3._Primary_Diagonal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            long[][] matrix = new long[sizeOfMatrix][];
+            long diagonalSum = 0;
+
+            for (int i = 0; i < sizeOfMatrix; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(long.Parse)
+                            .ToArray();
+                diagonalSum += matrix[i][i];
+            }
+
+            Console.WriteLine(diagonalSum);
         }
     }
 }
