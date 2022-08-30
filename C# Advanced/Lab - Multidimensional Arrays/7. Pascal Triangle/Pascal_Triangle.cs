6d1968cb548806bf8d87632923f577a466c0aecb
Gyuntek <gyuntekahmed@abv.bg>
2022-08-30 22:25:17 +0300

    Initial commit

--- C# Advanced/Lab - Multidimensional Arrays/7. Pascal Triangle/Pascal_Triangle.cs
+++ C# Advanced/Lab - Multidimensional Arrays/7. Pascal Triangle/Pascal_Triangle.cs
@@ -5,6 +5,28 @@ class Pascal_Triangle
 {
     static void Main()
     {
-        Console.WriteLine("Hi");
+        int n = int.Parse(Console.ReadLine());
+        long[][] triangle = new long[n][];
+
+        for (int row = 0; row < n; row++)
+        {
+            triangle[row] = new long[row + 1];
+            triangle[row][0] = 1;
+
+            for (int col = 1; col < row; col++)
+            {
+                triangle[row][col] =
+                    triangle[row - 1][col - 1] +
+                    triangle[row - 1][col];
+            }
+
+            triangle[row][row] = 1;
+        }
+
+        // Print the triangle
+        for (int row = 0; row < triangle.Length; row++)
+        {
+            Console.WriteLine(string.Join(" ", triangle[row]));
+        }
     }
 }
