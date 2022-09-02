9b8364f75d72a5b1e986b75147e246f814caeb4d
Gyuntek <gyuntekahmed@abv.bg>
2022-09-02 21:52:02 +0300

    Initial commit

---
+++ Programing Basic/First Steps In Coding - Exercise/01. USD to BGN/_USD_to_BGN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double leva = 1.79549;
+            usd = usd * leva;
+            Console.WriteLine(usd);
+        }
+    }
+}
---
+++ Programing Basic/First Steps In Coding - Exercise/02. Radians to Degrees/_Radians_to_Degrees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Radians_to_Degrees
+{
+    class _Radians_to_Degrees
+    {
+        static void Main(string[] args)
+        {
+            double radiani = double.Parse(Console.ReadLine());
+            double gradusi;
+            gradusi = radiani * 180 / 3.1415926535897932384626433;
+            Console.WriteLine(gradusi);
+
+        }
+    }
+}
