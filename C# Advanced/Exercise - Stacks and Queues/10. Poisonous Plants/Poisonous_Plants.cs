6da453d0d737a29bf79102f9ab570752d108fcb1
Gyuntek <gyuntekahmed@abv.bg>
2022-09-02 21:22:49 +0300

    Initial commit

--- C# Advanced/Exercise - Stacks and Queues/10. Poisonous Plants/Poisonous_Plants.cs
+++ C# Advanced/Exercise - Stacks and Queues/10. Poisonous Plants/Poisonous_Plants.cs
@@ -1,10 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Poisonous_Plants
 {
     static void Main()
     {
-        Console.WriteLine("Hello World!");
+        int numberPlants = int.Parse(Console.ReadLine());
+
+        List<int> plants = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        Stack<int> deadPlantsIndexes = new Stack<int>();
+
+        int dayCounter = 0;
+
+        while (true)
+        {
+            dayCounter++;
+
+            int lastDeadDay = 0;
+
+            for (int i = 0; i < numberPlants - 1; i++)
+            {
+                if (plants[i + 1] > plants[i])
+                {
+                    deadPlantsIndexes.Push(i + 1);
+
+                    lastDeadDay = 1;
+                }
+            }
+
+            int deadPlantsCount = deadPlantsIndexes.Count;
+
+            for (int i = 0; i < deadPlantsCount; i++)
+            {
+                plants.RemoveAt(deadPlantsIndexes.Pop());
+            }
+
+            numberPlants = plants.Count;
+
+            if (lastDeadDay == 0)
+            {
+                Console.WriteLine(dayCounter - 1);
+                return;
+            }
+        }
+
     }
 }
