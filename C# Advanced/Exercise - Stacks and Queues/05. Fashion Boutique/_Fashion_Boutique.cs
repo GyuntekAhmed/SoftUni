048ca8f7913f49967caeeaa08f69373012c450d9
Gyuntek <gyuntekahmed@abv.bg>
2022-09-04 22:17:01 +0300

    Initial commit

--- C# Advanced/Exercise - Stacks and Queues/05. Fashion Boutique/_Fashion_Boutique.cs
+++ C# Advanced/Exercise - Stacks and Queues/05. Fashion Boutique/_Fashion_Boutique.cs
@@ -2,41 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _05._Fashion_Boutique
+class _Fashion_Boutique
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Stack<int> clothesInBox = new Stack<int>(Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse));
+        int capacity = int.Parse(Console.ReadLine());
+
+        int racks = 1;
+        int stack = 0;
+
+        int countOfClothes = clothesInBox.Count;
+
+        for (int i = 0; i < countOfClothes; i++)
         {
-            Stack<int> clothesInBox = new Stack<int>(Console.ReadLine()
-                .Split(' ')
-                .Select(int.Parse));
-            int capacity = int.Parse(Console.ReadLine());
+            int currentCloth = clothesInBox.Peek();
 
-            int racks = 1;
-            int stack = 0;
-
-            int countOfClothes = clothesInBox.Count;
-
-            for (int i = 0; i < countOfClothes; i++)
+            if (stack + currentCloth <= capacity)
             {
-                int currentCloth = clothesInBox.Peek();
-
-                if (stack + currentCloth <= capacity)
-                {
-                    stack += currentCloth;
-                    clothesInBox.Pop();
-                }
-                else
-                {
-                    stack = 0;
-                    stack = currentCloth; 
-                    racks++;
-                    clothesInBox.Pop();
-                }
+                stack += currentCloth;
+                clothesInBox.Pop();
             }
-
-            Console.WriteLine(racks);
+            else
+            {
+                stack = 0;
+                stack = currentCloth;
+                racks++;
+                clothesInBox.Pop();
+            }
         }
+
+        Console.WriteLine(racks);
     }
 }
