a6974c4fb3faa7e42f2108a9acbcc847a7d8d63f
Gyuntek <gyuntekahmed@abv.bg>
2022-09-04 22:15:28 +0300

    Initial commit

--- C# Advanced/Exercise - Stacks and Queues/01. Basic Stack Operations/_Basic_Stack_Operations.cs
+++ C# Advanced/Exercise - Stacks and Queues/01. Basic Stack Operations/_Basic_Stack_Operations.cs
@@ -2,49 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _01._Basic_Stack_Operations
+class _Basic_Stack_Operations
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Stack<int> stack = new Stack<int>();
+
+        int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        int n = numbers[0];
+        int s = numbers[1];
+        int x = numbers[2];
+
+        List<int> numberList = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+        for (int i = 0; i < n; i++)
         {
-            Stack<int> stack = new Stack<int>();
-
-            int[] numbers = Console.ReadLine()
-                .Split()
-                .Select(int.Parse)
-                .ToArray();
-            int n = numbers[0];
-            int s = numbers[1];
-            int x = numbers[2];
-
-            List<int> numberList = Console.ReadLine()
-                .Split(' ')
-                .Select(int.Parse)
-                .ToList();
-            for (int i = 0; i < n; i++)
-            {
-                stack.Push(numberList[i]);
-            }
-
-            for (int j = 0; j < s; j++)
-            {
-                stack.Pop();
-            }
-
-            if (stack.Count == 0)
-            {
-                Console.WriteLine(0);
-            }
-            else if (stack.Contains(x))
-            {
-                Console.WriteLine("true");
-            }
-            else
-            {
-                Console.WriteLine(stack.Min());
-            }
-
+            stack.Push(numberList[i]);
         }
+
+        for (int j = 0; j < s; j++)
+        {
+            stack.Pop();
+        }
+
+        if (stack.Count == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else if (stack.Contains(x))
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine(stack.Min());
+        }
+
     }
 }
