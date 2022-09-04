59294eba4acdccedbde10c8e1fde7bc96f8b4b6e
Gyuntek <gyuntekahmed@abv.bg>
2022-09-04 22:18:15 +0300

    Initial commit

---
+++ C# Advanced/Exercise - Stacks and Queues/08. Balanced Parenthesis/Balanced_Parenthesis.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Balanced_Parenthesis
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        bool isBalanced = false;
+        Stack<char> openBracket = new Stack<char>();
+
+
+        foreach (char bracket in input)
+        {
+            if (bracket == '(' || bracket == '{' || bracket == '[')
+            {
+                openBracket.Push(bracket);
+            }
+            else if (bracket == ')' || bracket == '}' || bracket == ']')
+            {
+                if (openBracket.Count == 0)
+                {
+                    isBalanced = false;
+                    break;
+                }
+
+                char lastOpen = openBracket.Pop();
+
+                if (lastOpen == '(' && bracket == ')')
+                {
+                    isBalanced = true;
+                }
+                else if (lastOpen == '{' && bracket == '}')
+                {
+                    isBalanced = true;
+                }
+                else if (lastOpen == '[' && bracket == ']')
+                {
+                    isBalanced = true;
+                }
+                else
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+        if (isBalanced)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
