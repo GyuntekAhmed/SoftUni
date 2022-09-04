72174274f08ffc6843dac74565fc84ad5e01a40d
Gyuntek <gyuntekahmed@abv.bg>
2022-09-04 22:16:34 +0300

    Initial commit

--- C# Advanced/Exercise - Stacks and Queues/04. Fast Food/_Fast_Food.cs
+++ C# Advanced/Exercise - Stacks and Queues/04. Fast Food/_Fast_Food.cs
@@ -2,41 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _04._Fast_Food
+class _Fast_Food
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int quantityOfFood = int.Parse(Console.ReadLine());
+
+        Queue<int> ordersQueue = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+
+        Console.WriteLine(ordersQueue.Max());
+
+        int countOfOrders = ordersQueue.Count;
+
+        for (int order = 1; order <= countOfOrders; order++)
         {
-            int quantityOfFood = int.Parse(Console.ReadLine());
-
-            Queue<int> ordersQueue = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
-
-            Console.WriteLine(ordersQueue.Max());
-
-            int countOfOrders = ordersQueue.Count;
-
-            for (int order = 1; order <= countOfOrders; order++)
+            if (quantityOfFood >= ordersQueue.Peek())
             {
-                if (quantityOfFood >= ordersQueue.Peek())
-                {
-                    quantityOfFood -= ordersQueue.Peek();
-                    ordersQueue.Dequeue();
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            if (ordersQueue.Count == 0)
-            {
-                Console.WriteLine("Orders complete");
+                quantityOfFood -= ordersQueue.Peek();
+                ordersQueue.Dequeue();
             }
             else
             {
-                Console.WriteLine($"Orders left: " + string.Join(" ", ordersQueue));
+                break;
             }
+        }
+
+        if (ordersQueue.Count == 0)
+        {
+            Console.WriteLine("Orders complete");
+        }
+        else
+        {
+            Console.WriteLine($"Orders left: " + string.Join(" ", ordersQueue));
         }
     }
 }
