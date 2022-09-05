d0d6f27e5674b1922368b200dd6cfceabf9b8966
Gyuntek <gyuntekahmed@abv.bg>
2022-09-05 21:24:21 +0300

    Initial commit

---
+++ C# Advanced/Lab - Stacks and Queues/2. Stack Sum/_Stack_Sum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class _Stack_Sum
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split()
+            .Select(int.Parse)
+            .ToArray();
+        Stack<int> stack = new Stack<int>(numbers);
+
+        //foreach (int number in numbers)
+        //{
+        //    stack.Push(number);
+        //}
+        while (true)
+        {
+            string[] command = Console.ReadLine()
+                .ToLower()
+                .Split();
+
+            string tokens = command[0];
+            if (tokens == "end")
+            {
+                break;
+            }
+
+            switch (tokens)
+            {
+                case "add":
+                    int firstNumber = int.Parse(command[1]);
+                    int secondNumber = int.Parse(command[2]);
+                    stack.Push(firstNumber);
+                    stack.Push(secondNumber);
+                    break;
+                case "remove":
+                    int count = int.Parse(command[1]);
+                    if (stack.Count < count)
+                    {
+                        break;
+                    }
+                    for (int i = 0; i < count; i++)
+                    {
+
+                        stack.Pop();
+                    }
+                    break;
+            }
+        }
+        var sum = stack.Sum();
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
