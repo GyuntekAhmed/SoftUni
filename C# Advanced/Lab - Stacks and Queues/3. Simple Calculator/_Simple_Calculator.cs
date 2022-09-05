b1c1de90ffe82a8b0d0ebfbb4811b94475334316
Gyuntek <gyuntekahmed@abv.bg>
2022-09-05 21:24:49 +0300

    Initial commit

---
+++ C# Advanced/Lab - Stacks and Queues/3. Simple Calculator/_Simple_Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class _Simple_Calculator
+{
+    static void Main()
+    {
+        Stack<int> stack = new Stack<int>();
+        string[] input = Console.ReadLine().Split();
+
+        int sum = int.Parse(input[0]);
+
+        for (int i = 1; i < input.Length; i++)
+        {
+
+            if (input[i] == "+")
+            {
+                int number = int.Parse(input[i + 1]);
+                sum += number;
+            }
+            else if (input[i] == "-")
+            {
+                int number = int.Parse(input[i + 1]);
+                sum -= number;
+            }
+
+        }
+        stack.Push(sum);
+
+        Console.WriteLine(string.Join("", stack));
+    }
+}
