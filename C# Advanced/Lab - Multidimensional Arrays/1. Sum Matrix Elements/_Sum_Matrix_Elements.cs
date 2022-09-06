7873c870e3b411b30048cc202206b5460d8f8488
Gyuntek <gyuntekahmed@abv.bg>
2022-09-06 21:39:05 +0300

    Initial commit

---
+++ C# Advanced/Lab - Multidimensional Arrays/1. Sum Matrix Elements/_Sum_Matrix_Elements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+class _Sum_Matrix_Elements
+{
+    static void Main()
+    {
+        int[] dimensions = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+        (int rowsCount, int colsCount) = (dimensions[0], dimensions[1]);
+
+        int[,] matrix = new int[rowsCount, colsCount];
+
+        for (int row = 0; row < rowsCount; row++)
+        {
+            int[] line = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            for (int col = 0; col < colsCount; col++)
+            {
+                matrix[row, col] = line[col];
+            }
+        }
+
+        long sum = 0;
+
+        for (int row = 0; row < rowsCount; row++)
+            for (int col = 0; col < colsCount; col++)
+                sum += matrix[row, col];
+        Console.WriteLine(rowsCount);
+        Console.WriteLine(colsCount);
+        Console.WriteLine(sum);
+    }
+}
