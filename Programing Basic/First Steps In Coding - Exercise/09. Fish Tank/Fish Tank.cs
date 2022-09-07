dfb789845ebab9d438bd566f89f17d8f11cbfaa4
Gyuntek <gyuntekahmed@abv.bg>
2022-09-07 23:30:19 +0300

    Initial commit

---
+++ Programing Basic/First Steps In Coding - Exercise/09. Fish Tank/Fish Tank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Fish_Tank
+{
+    static void Main()
+    {
+        double decimeterDivider = 10;
+        double percentDivider = 100;
+
+        int lenghtInCm = int.Parse(Console.ReadLine());
+        int widthInCm = int.Parse(Console.ReadLine());
+        int heightInCm = int.Parse(Console.ReadLine());
+        double accessoriesVolumeNum = double.Parse(Console.ReadLine());
+
+
+        double lenghtInDm = lenghtInCm / decimeterDivider;
+        double widthInDm = widthInCm / decimeterDivider;
+        double heightInDm = heightInCm / decimeterDivider;
+
+        double accessoriesVolumePercentage = accessoriesVolumeNum / percentDivider;
+
+        double totalVolume = lenghtInDm * widthInDm * heightInDm;
+
+        double accessoriesVolume = totalVolume * accessoriesVolumePercentage;
+        double watterVolume = totalVolume - accessoriesVolume;
+        Console.WriteLine(watterVolume);
+    }
+}
