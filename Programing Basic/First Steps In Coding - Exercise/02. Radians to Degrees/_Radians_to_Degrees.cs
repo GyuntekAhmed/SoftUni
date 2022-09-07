0928ccdeff3c723c7a129100e9b4c61f85285153
Gyuntek <gyuntekahmed@abv.bg>
2022-09-07 23:08:50 +0300

    Initial commit

--- Programing Basic/First Steps In Coding - Exercise/02. Radians to Degrees/_Radians_to_Degrees.cs
+++ Programing Basic/First Steps In Coding - Exercise/02. Radians to Degrees/_Radians_to_Degrees.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 
-namespace _02._Radians_to_Degrees
+class Radians_to_Degrees
 {
-    class _Radians_to_Degrees
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            double radiani = double.Parse(Console.ReadLine());
-            double gradusi;
-            gradusi = radiani * 180 / 3.1415926535897932384626433;
-            Console.WriteLine(gradusi);
+        double radiani = double.Parse(Console.ReadLine());
+        double gradusi;
+        gradusi = radiani * 180 / 3.1415926535897932384626433;
+        Console.WriteLine(gradusi);
 
-        }
     }
 }
