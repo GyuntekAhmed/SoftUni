c016fc9d3a6b254261c3575a43baf878aa57f2b4
Gyuntek <gyuntekahmed@abv.bg>
2022-09-07 22:48:46 +0300

    Initial commit

---
+++ Fundamentals/Exercise - Regular Expressions/06. Extract Emails/Extract_Emails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+    class Extract_Emails
+{
+    public static object MatchColection { get; private set; }
+
+    static void Main(string[] args)
+    {
+        string pattern = @"(^|\s)[A-Za-z0-9][\w+\-\.]*[A-Za-z0-9]@[A-Za-z]+([.-][A-Za-z]+)+\b";
+
+        string input = Console.ReadLine();
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+        matches.ToList().ForEach(Console.WriteLine);
+    }
+}
