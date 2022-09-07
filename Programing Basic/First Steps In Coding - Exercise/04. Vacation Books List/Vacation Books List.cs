63a93d8d870e9c015bd34f5b1b1fe9ab05ac99cd
Gyuntek <gyuntekahmed@abv.bg>
2022-09-07 23:13:00 +0300

    Initial commit

---
+++ Programing Basic/First Steps In Coding - Exercise/04. Vacation Books List/Vacation Books List.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Vacation_Books_List
+
+{
+    static void Main()
+    {
+        int brstr = int.Parse(Console.ReadLine());
+        int strcht = int.Parse(Console.ReadLine());
+        int brdni = int.Parse(Console.ReadLine());
+        int brchas;
+        int neobhch;
+        brchas = brstr / strcht;
+        neobhch = brchas / brdni;
+        Console.WriteLine(neobhch);
+    }
+}
