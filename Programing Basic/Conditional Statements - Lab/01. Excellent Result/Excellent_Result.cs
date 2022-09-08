d0f0ae50db6b710d79275403cc4b531ce1ebdfbf
Gyuntek <gyuntekahmed@abv.bg>
2022-09-08 21:58:23 +0300

    Initial commit

---
+++ Programing Basic/Conditional Statements - Lab/01. Excellent Result/Excellent_Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+        if (grade >= 5.50)
+        {
+            Console.WriteLine("Excellent!");
+        }
+    }
