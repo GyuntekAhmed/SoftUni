628c6530458cfdeb4b15d98b62619cbfd76bdcd3
Gyuntek <gyuntekahmed@abv.bg>
2022-09-08 22:08:51 +0300

    Initial commit

---
+++ Programing Basic/Conditional Statements - Lab/03. Even or Odd/Even_or_Odd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
+    }
+}
