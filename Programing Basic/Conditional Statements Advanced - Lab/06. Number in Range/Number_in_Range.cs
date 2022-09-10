605b1fbd1c03ea798e25e88e5e70e55095dc9612
Gyuntek <gyuntekahmed@abv.bg>
2022-09-10 21:04:58 +0300

    Initial commit

---
+++ Programing Basic/Conditional Statements Advanced - Lab/06. Number in Range/Number_in_Range.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Number_in_Range
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number >= -100)
+        {
+            if (number <= 100)
+
+                if (number != 0)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
