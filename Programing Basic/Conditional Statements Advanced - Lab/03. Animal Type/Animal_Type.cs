b997f492dabc1eaf9859578eb0b7a742b366a989
Gyuntek <gyuntekahmed@abv.bg>
2022-09-10 20:59:44 +0300

    Initial commit

---
+++ Programing Basic/Conditional Statements Advanced - Lab/03. Animal Type/Animal_Type.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Animal_Type
+{
+    static void Main()
+    {
+        //            1.dog->mammal
+        //2.crocodile, tortoise, snake->reptile
+        //3.others->unknown
+
+        string animal = Console.ReadLine();
+
+        switch (animal)
+        {
+            case "dog":
+                Console.WriteLine("mammal");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("reptile");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+    }
+}
