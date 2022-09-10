72d3d9b414869954168a121f072a52db97f1a642
Gyuntek <gyuntekahmed@abv.bg>
2022-09-10 20:57:52 +0300

    Initial commit

---
+++ Programing Basic/Conditional Statements Advanced - Lab/02.Weekend or Working Day/Weekend_or_Working_Day.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Weekend_or_Working_Day
+{
+    static void Main()
+    {
+        //   1   Monday
+        //   2   Tuesday
+        //   3   Wednesday
+        //   4   Thursday
+        //   5   Friday
+        //   6   Saturday
+        //   7   Sunday
+        //  - 1  Error
+
+        string day = Console.ReadLine();
+
+        switch (day)
+        {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                Console.WriteLine("Working day");
+                break;
+            case "Saturday":
+            case "Sunday":
+                Console.WriteLine("Weekend");
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
+    }
+}
