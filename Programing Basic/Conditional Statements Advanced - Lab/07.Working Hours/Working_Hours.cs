b778e1cf4e88fd92615cb7b1bf4264e2bd477dcc
Gyuntek <gyuntekahmed@abv.bg>
2022-09-10 21:06:43 +0300

    Initial commit

---
+++ Programing Basic/Conditional Statements Advanced - Lab/07.Working Hours/Working_Hours.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Working_Hours
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        string day = Console.ReadLine();
+
+        if (hours >= 10 && hours <= 18)
+        {
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    Console.WriteLine("open");
+                    break;
+                default:
+                    Console.WriteLine("closed");
+                    break;
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("closed");
+        }
+    }
+}
