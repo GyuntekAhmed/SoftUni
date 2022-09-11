eae0d7aa753b7a2026a927aab5fd5dba527fdf2a
Gyuntek <gyuntekahmed@abv.bg>
2022-09-11 11:00:51 +0300

    Initial commit

---
+++ C# Advanced/Exercise - Multidimensional Arrays/2. Squares in Matrix/Squares_in_Matrix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+class Squares_in_Matrix
+{
+    static void Main()
+    {
+        int[] sizeOfMatrix = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        int rows = sizeOfMatrix[0];
+        int cols = sizeOfMatrix[1];
+
+        string[,] matrix = new string[rows, cols];
+
+        FillMatrix(matrix);
+
+        int count = 0;
+
+        for (int row = 0; row < rows - 1; row++)
+        {
+            for (int col = 0; col < cols - 1; col++)
+            {
+                if (matrix[row, col] == matrix[row, col + 1]
+                    && matrix[row, col] == matrix[row + 1, col]
+                    && matrix[row, col] == matrix[row + 1, col + 1])
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+    public static void FillMatrix(string[,] matrix)
+    {
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            string[] rowData = Console.ReadLine().Split();
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = rowData[col];
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exercise - Multidimensional Arrays/Method for FillMatrix/Method_for_FillMatrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+internal class Method_for_FillMatrix
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] array = new int[n, n];
+
+        FillMatrix(array, " ");
+
+    }
+
+    public static void FillMatrix(int[,] array, string spliter = " ")
+    {
+        for (int row = 0; row < array.GetLength(0); row++)
+        {
+            int[] rowData = Console.ReadLine()
+                .Split(spliter, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            for (int col = 0; col < array.GetLength(1); col++)
+            {
+                array[row, col] = rowData[col];
+            }
+        }
+    }
+}
