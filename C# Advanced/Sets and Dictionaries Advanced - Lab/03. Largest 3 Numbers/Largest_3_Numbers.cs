40f1b40527305e76025a0ac342c3209bea43b4a4
Gyuntek <gyuntekahmed@abv.bg>
2022-09-17 00:00:14 +0300

    Initial commit

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Largest_3_Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Largest_3_Numbers
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        int[] sortedNums = numbers.OrderByDescending(n => n).ToArray();
+
+        int countToPrint = sortedNums.Length;
+
+        if (countToPrint > 3)
+        {
+            countToPrint = 3;
+        }
+
+        for (int i = 0; i < countToPrint; i++)
+        {
+            Console.Write($"{sortedNums[i]} ");
+        }
+    }
+}
