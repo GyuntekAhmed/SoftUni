2aa07056d5b6fc7564e93af59b0fd02429af045c
Gyuntek <gyuntekahmed@abv.bg>
2022-09-16 22:30:45 +0300

    Initial commit

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Count_Same_Values_in_Array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Count_Same_Values_in_Array
+{
+    static void Main()
+    {
+        double[] numbers = Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .ToArray();
+
+        Dictionary<double, int> countByNum = new Dictionary<double, int>();
+
+        foreach (double number in numbers)
+        {
+            if (countByNum.ContainsKey(number))
+            {
+                countByNum[number]++;
+            }
+            else
+            {
+                countByNum[number] = 1;
+            }
+        }
+
+        foreach (var pair in countByNum)
+        {
+            Console.WriteLine($"{pair.Key} - {pair.Value} times");
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Average_Student_Grades.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Average_Student_Grades
+{
+    static void Main()
+    {
+        Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+        int countOfStudents = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countOfStudents; i++)
+        {
+            string[] infoAboutStudent = Console.ReadLine().Split(' ');
+            string name = infoAboutStudent[0];
+            double grade = double.Parse(infoAboutStudent[1]);
+            
+            students.Add(name, new List<double>());
+            students["name"].Add(grade);
+        }
+
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Key} -> {string.Join(' ', student.Value)} (avg: {student.Value.Average():f2})");
+        }
+    }
+}
