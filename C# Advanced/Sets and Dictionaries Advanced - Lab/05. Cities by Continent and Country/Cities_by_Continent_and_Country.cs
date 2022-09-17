93e80bd1fa668318cdc74f0481569e82d6a9ea5e
Gyuntek <gyuntekahmed@abv.bg>
2022-09-17 22:49:44 +0300

    Initial commit

--- C# Advanced/Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Cities_by_Continent_and_Country.cs
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Cities_by_Continent_and_Country.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Cities_by_Continent_and_Country
 {
     static void Main()
     {
+        var Countries = new Dictionary<string, Dictionary<string, List<string>>>();
 
+        int countToAdd = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countToAdd; i++)
+        {
+            string[] spliter = Console.ReadLine().Split();
+
+            string continent = spliter[0];
+            string country = spliter[1];
+            string city = spliter[2];
+
+            if (!Countries.ContainsKey(continent))
+            {
+                Countries.Add(continent, new Dictionary<string, List<string>>());
+            }
+            if (!Countries[continent].ContainsKey(country))
+            {
+                Countries[continent].Add(country, new List<string>());
+            }
+
+            Countries[continent][country].Add(city);
+        }
+
+        foreach (var continent in Countries)
+        {
+            Console.WriteLine($"{continent.Key}:");
+
+            foreach (var country in continent.Value)
+            {
+                Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+            }
+        }
     }
 }
