6fcd2ec1c67937fa7151b112af23575eb0c51e78
Gyuntek <gyuntekahmed@abv.bg>
2022-09-18 22:56:11 +0300

    Initial commit

---
+++ C# Advanced/Exercises - Sets and Dictionaries Advanced/03. Periodic Table/Periodic_Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Periodic_Table
+{
+    static void Main()
+    {
+        SortedSet<string> set = new SortedSet<string>();
+
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                string currentStr = input[j];
+
+                set.Add(currentStr);
+            }
+        }
+
+        foreach (string item in set)
+        {
+            Console.Write($"{item} ");
+        }
+    }
+}
