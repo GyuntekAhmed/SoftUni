7c4323212950c79e652d92487bae04c5b4a88fe3
Gyuntek <gyuntekahmed@abv.bg>
2022-09-18 10:23:18 +0300

    Initial commit

--- C# Advanced/Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Record_Unique_Names.cs
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Record_Unique_Names.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Record_Unique_Names
 {
     static void Main()
     {
+        HashSet<string> names = new HashSet<string>();
 
+        int countToAdd = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countToAdd; i++)
+        {
+            string name = Console.ReadLine();
+
+            names.Add(name);
+        }
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
     }
 }
---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/07. Parking Lot/Parking_Lot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Parking_Lot
+{
+    static void Main()
+    {
+        HashSet<string> cars = new HashSet<string>();
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+
+            if (command == "END")
+            {
+                PrintCars(cars);
+                break;
+            }
+
+            string[] splited = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string action = splited[0];
+            string car = splited[1];
+
+            AddCars(cars, car, action);
+        }
+    }
+
+    static void AddCars(HashSet<string> cars, string car, string action)
+    {
+        if (action == "IN")
+        {
+            cars.Add(car);
+        }
+        if (action == "OUT")
+        {
+            cars.Remove(car);
+        }
+
+    }
+
+    static void PrintCars(HashSet<string> cars)
+    {
+        foreach (string car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
