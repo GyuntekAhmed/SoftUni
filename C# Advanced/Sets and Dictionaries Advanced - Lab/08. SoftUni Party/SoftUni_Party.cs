263bea45bd674d1648f13f98b2d1c42f1250dc26
Gyuntek <gyuntekahmed@abv.bg>
2022-09-18 22:26:16 +0300

    Initial commit

--- C# Advanced/Sets and Dictionaries Advanced - Lab/08. SoftUni Party/SoftUni_Party.cs
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/08. SoftUni Party/SoftUni_Party.cs
@@ -1,10 +1,59 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 class SoftUni_Party
 {
     static void Main()
     {
+        HashSet<string> set = new HashSet<string>();
 
+        while (true)
+        {
+            string command = Console.ReadLine();
+
+            if (command == "PARTY")
+            {
+                break;
+            }
+
+            set.Add(command);
+        }
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "END")
+            {
+                break;
+            }
+
+            if (set.Contains(input))
+            {
+                set.Remove(input);
+            }
+        }
+
+        Console.WriteLine(set.Count);
+
+        foreach (string item in set)
+        {
+            char[] ch = item.ToCharArray();
+
+            if (char.IsDigit(ch[0]))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        foreach (string item in set)
+        {
+            char[] ch = item.ToCharArray();
+
+            if (char.IsLetter(ch[0]))
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
