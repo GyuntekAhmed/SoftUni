654e9140078665c9eb85abdf1e6d366c3dba61bb
Gyuntek <gyuntekahmed@abv.bg>
2022-09-18 22:31:29 +0300

    Initial commit

---
+++ C# Advanced/Exercises - Sets and Dictionaries Advanced/01. Unique Usernames/Unique_Usernames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Unique_Usernames
+{
+    static void Main()
+    {
+        HashSet<string> names = new HashSet<string>();
+
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            string name = Console.ReadLine();
+
+            names.Add(name);
+        }
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
