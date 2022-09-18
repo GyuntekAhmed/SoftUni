47d97618e115e36e085a80a132687c60c88520cd
Gyuntek <gyuntekahmed@abv.bg>
2022-09-18 22:41:54 +0300

    Initial commit

---
+++ C# Advanced/Exercises - Sets and Dictionaries Advanced/02. Sets of Elements/Sets_of_Elements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Sets_of_Elements
+{
+    static void Main()
+    {
+        HashSet<int> firstSet = new HashSet<int>();
+        HashSet<int> secondSet = new HashSet<int>();
+
+        int[] input = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        int n = input[0];
+        int m = input[1];
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            firstSet.Add(number);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            secondSet.Add(number);
+        }
+
+        foreach (int firstNumber in firstSet)
+        {
+            foreach (int secondNumber in secondSet)
+            {
+                if (secondNumber == firstNumber)
+                {
+                    Console.Write($"{firstNumber} ");
+                }
+            }
+        }
+    }
+}
