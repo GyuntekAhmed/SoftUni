f2979120b305d8ee1c06194c09a27845f8c4a7b1
Gyuntek <gyuntekahmed@abv.bg>
2022-09-18 23:30:30 +0300

    Initial commit

---
+++ C# Advanced/Exercises - Sets and Dictionaries Advanced/05. Count Symbols/Count_Symbols.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Count_Symbols
+{
+    static void Main()
+    {
+        SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+        string text = Console.ReadLine();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            char currentChar = text[i];
+
+            if (symbols.ContainsKey(currentChar))
+            {
+                symbols[currentChar]++;
+            }
+            else
+            {
+                symbols.Add(currentChar, 1);
+            }
+        }
+
+        foreach (var item in symbols)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value} time/s");
+        }
+    }
+}
