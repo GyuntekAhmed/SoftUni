c46298ee5b0502e5172830e9ea93db70adab3c3a
Gyuntek <gyuntekahmed@abv.bg>
2022-09-18 23:21:16 +0300

    Initial commit

---
+++ C# Advanced/Exercises - Sets and Dictionaries Advanced/04. Even Times/Even_Times.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Even_Times
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (numbers.ContainsKey(number))
+            {
+                numbers[number]++;
+            }
+            else
+            {
+                numbers.Add(number, 1);
+            }
+        }
+
+        var evenNumber = numbers.First(n => n.Value % 2 == 0).Key;
+
+        Console.WriteLine(evenNumber);
+    }
+}
