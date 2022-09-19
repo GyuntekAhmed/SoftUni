00434bc0ae585db9e73418d4caee8b16485a280c
Gyuntek <gyuntekahmed@abv.bg>
2022-09-19 22:22:46 +0300

    Initial commit

---
+++ Programing Basic/Conditional Statements Advanced - Exercise/06. Operations Between Numbers/Operations_Between_Numbers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+class Operations_Between_Numbers
+{
+    static void Main()
+    {
+        int numOne = int.Parse(Console.ReadLine());
+        int numTwo = int.Parse(Console.ReadLine());
+        char operation = char.Parse(Console.ReadLine());
+        double results = 0;
+        string finalResult = null;
+
+        switch (operation)
+        {
+            case '+':
+                results = numOne + numTwo;
+                if (results % 2 == 0)
+                {
+                    finalResult = $"{numOne} {operation} {numTwo} = {results} - even";
+                }
+                else
+                {
+                    finalResult = $"{numOne} {operation} {numTwo} = {results} - odd";
+                }
+                break;
+            case '-':
+                results = numOne - numTwo;
+                if (results % 2 == 0)
+                {
+                    finalResult = $"{numOne} {operation} {numTwo} = {results} - even";
+                }
+                else
+                {
+                    finalResult = $"{numOne} {operation} {numTwo} = {results} - odd";
+                }
+                break;
+            case '*':
+                results = numOne * numTwo;
+                if (results % 2 == 0)
+                {
+                    finalResult = $"{numOne} {operation} {numTwo} = {results} - even";
+                }
+                else
+                {
+                    finalResult = $"{numOne} {operation} {numTwo} = {results} - odd";
+                }
+                break;
+            case '/':
+                if (numTwo == 0)
+                {
+                    finalResult = $"Cannot divide {numOne} by zero";
+                }
+                else
+                {
+                    results = numOne / (double)numTwo;
+                    finalResult = $"{numOne} {operation} {numTwo} = {results:f2}";
+                }
+                break;
+            case '%':
+                if (numTwo == 0)
+                {
+                    finalResult = $"Cannot divide {numOne} by zero";
+                }
+                else
+                {
+                    results = numOne % numTwo;
+                    finalResult = $"{numOne} {operation} {numTwo} = {results}";
+                }
+                break;
+
+        }
+        Console.WriteLine(finalResult);
+    }
+}
