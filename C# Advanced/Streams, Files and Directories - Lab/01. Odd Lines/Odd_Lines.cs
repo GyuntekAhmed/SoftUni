1e53f0214dc3b2bf78306bab2ea16898707a5e3d
Gyuntek <gyuntekahmed@abv.bg>
2022-09-21 21:16:12 +0300

    Initial commit

---
+++ C# Advanced/Streams, Files and Directories - Lab/01. Odd Lines/Odd_Lines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Odd_Lines
+{
+    static void Main()
+    {
+        var reader = new StreamReader("../../../input.txt");
+        using (reader)
+        {
+            var writer = new StreamWriter("../../../output.txt");
+            using(writer)
+            {
+                int lineNum = 0;
+
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    if (lineNum % 2 == 1)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
