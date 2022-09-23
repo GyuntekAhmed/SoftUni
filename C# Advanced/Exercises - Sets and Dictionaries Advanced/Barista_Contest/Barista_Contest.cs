5aac095e366e9901294aef74a74ff0366488f47b
Gyuntek <gyuntekahmed@abv.bg>
2022-09-23 22:47:28 +0300

    Initial commit

--- C# Advanced/Exercises - Sets and Dictionaries Advanced/Barista_Contest/Barista_Contest.cs
+++ C# Advanced/Exercises - Sets and Dictionaries Advanced/Barista_Contest/Barista_Contest.cs
@@ -15,6 +15,11 @@ class Barista_Contest
         //   Latte                200
 
         SortedDictionary<string, int> cofeeDrinks = new SortedDictionary<string, int>();
+        cofeeDrinks.Add("Cortado", 0);
+        cofeeDrinks.Add("Espresso", 0);
+        cofeeDrinks.Add("Capuccino", 0);
+        cofeeDrinks.Add("Americano", 0);
+        cofeeDrinks.Add("Latte", 0);
 
         Queue<int> coffeeQuantities = new Queue<int>(Console.ReadLine()
             .Split(", ", StringSplitOptions.RemoveEmptyEntries)
@@ -24,7 +29,6 @@ class Barista_Contest
             .Split(", ", StringSplitOptions.RemoveEmptyEntries)
             .Select(int.Parse)
             .ToArray());
-
         for (int coffee = 0; coffee < coffeeQuantities.Count; coffee++)
         {
             for (int milk = milkQuantities.Count - 1; milk >= 0; milk--)
@@ -33,11 +37,40 @@ class Barista_Contest
                 int currentMilk = milkQuantities.Peek();
                 int sum = currentCoffee + currentMilk;
 
-                if (sum == 50)
+                switch (sum)
                 {
-
+                    case 50:
+                        cofeeDrinks["Cortado"]++;
+                        coffeeQuantities.Dequeue();
+                        milkQuantities.Pop();
+                        break;
+                    case 75:
+                        cofeeDrinks["Espresso"]++;
+                        coffeeQuantities.Dequeue();
+                        milkQuantities.Pop();
+                        break;
+                    case 100:
+                        cofeeDrinks["Capuccino"]++;
+                        coffeeQuantities.Dequeue();
+                        milkQuantities.Pop();
+                        break;
+                    case 150:
+                        cofeeDrinks["Americano"]++;
+                        coffeeQuantities.Dequeue();
+                        milkQuantities.Pop();
+                        break;
+                    case 200:
+                        cofeeDrinks["Latte"]++;
+                        coffeeQuantities.Dequeue();
+                        milkQuantities.Pop();
+                        break;
+                    default:
+                        coffeeQuantities.Dequeue();
+                        currentMilk -= 5;
+                        break;
                 }
             }
         }
+        Console.WriteLine();
     }
 }
