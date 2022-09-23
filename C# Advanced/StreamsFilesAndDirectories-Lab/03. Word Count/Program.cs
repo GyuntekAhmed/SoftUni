a7573612112e3fcc769cb35ae2e2503087584d94
Gyuntek <gyuntekahmed@abv.bg>
2022-09-23 22:09:53 +0300

    Initial commit

---
+++ C# Advanced/StreamsFilesAndDirectories-Lab/03. Word Count/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new SortedDictionary<string, int>();
+            string inputWords = File.ReadAllText("words.txt");
+            string[] words = inputWords.Split();
+            using var writer = new StreamWriter("output.txt");
+
+            using (var reader = new StreamReader("text.txt"))
+            {
+                string currentSentence = reader.ReadLine();
+
+                while (currentSentence != null)
+                {
+                    foreach (var word in words)
+                    {
+                        if (currentSentence.ToLower().Contains(word))
+                        {
+
+                            if (!dictionary.ContainsKey(word))
+                            {
+                                dictionary.Add(word, 0);
+                                dictionary[word]++;
+                            }
+                            else
+                            {
+                                dictionary[word]++;
+                            }
+                        }
+                    }
+
+                    currentSentence = reader.ReadLine();
+                }
+
+                foreach (var word in dictionary.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}
