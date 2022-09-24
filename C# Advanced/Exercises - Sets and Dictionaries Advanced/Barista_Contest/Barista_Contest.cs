f06f54bc054e1757886b09d90d96f7af2a1878d1
Gyuntek <gyuntekahmed@abv.bg>
2022-09-24 20:20:39 +0300

    Initial commit

--- C# Advanced/Exercises - Sets and Dictionaries Advanced/Barista_Contest/Barista_Contest.cs
+++ C# Advanced/Exercises - Sets and Dictionaries Advanced/Barista_Contest/Barista_Contest.cs
@@ -14,63 +14,120 @@ class Barista_Contest
         //  Americano             150
         //   Latte                200
 
-        SortedDictionary<string, int> cofeeDrinks = new SortedDictionary<string, int>();
+        Dictionary<string, int> cofeeDrinks = new Dictionary<string, int>();
         cofeeDrinks.Add("Cortado", 0);
         cofeeDrinks.Add("Espresso", 0);
         cofeeDrinks.Add("Capuccino", 0);
         cofeeDrinks.Add("Americano", 0);
         cofeeDrinks.Add("Latte", 0);
 
-        Queue<int> coffeeQuantities = new Queue<int>(Console.ReadLine()
+        List<int> coffeeQuantities = new List<int>(Console.ReadLine()
             .Split(", ", StringSplitOptions.RemoveEmptyEntries)
             .Select(int.Parse)
-            .ToArray());
+            .ToList());
         Stack<int> milkQuantities = new Stack<int>(Console.ReadLine()
             .Split(", ", StringSplitOptions.RemoveEmptyEntries)
             .Select(int.Parse)
             .ToArray());
-        for (int coffee = 0; coffee < coffeeQuantities.Count; coffee++)
-        {
-            for (int milk = milkQuantities.Count - 1; milk >= 0; milk--)
-            {
-                int currentCoffee = coffeeQuantities.Peek();
-                int currentMilk = milkQuantities.Peek();
-                int sum = currentCoffee + currentMilk;
+        bool defaultSum = false;
+        bool isOne = false;
+        int sum = 0;
 
-                switch (sum)
+        while (coffeeQuantities.Count > 0)
+        {
+            if (milkQuantities.Count == 0)
+            {
+                break;
+            }
+            if (!defaultSum)
+            {
+                sum = coffeeQuantities[0] + milkQuantities.Peek();
+            }
+            else
+            {
+                coffeeQuantities.Remove(coffeeQuantities[0]);
+                sum = milkQuantities.Peek() - 5;
+                defaultSum = false;
+                if (!isOne)
                 {
-                    case 50:
-                        cofeeDrinks["Cortado"]++;
-                        coffeeQuantities.Dequeue();
-                        milkQuantities.Pop();
-                        break;
-                    case 75:
-                        cofeeDrinks["Espresso"]++;
-                        coffeeQuantities.Dequeue();
-                        milkQuantities.Pop();
-                        break;
-                    case 100:
-                        cofeeDrinks["Capuccino"]++;
-                        coffeeQuantities.Dequeue();
-                        milkQuantities.Pop();
-                        break;
-                    case 150:
-                        cofeeDrinks["Americano"]++;
-                        coffeeQuantities.Dequeue();
-                        milkQuantities.Pop();
-                        break;
-                    case 200:
-                        cofeeDrinks["Latte"]++;
-                        coffeeQuantities.Dequeue();
-                        milkQuantities.Pop();
-                        break;
-                    default:
-                        coffeeQuantities.Dequeue();
-                        currentMilk -= 5;
-                        break;
+                    sum += coffeeQuantities[0];
                 }
             }
+
+
+            switch (sum)
+            {
+                case 50:
+                    cofeeDrinks["Cortado"]++;
+                    coffeeQuantities.Remove(coffeeQuantities[0]);
+                    milkQuantities.Pop();
+                    break;
+                case 75:
+                    cofeeDrinks["Espresso"]++;
+                    coffeeQuantities.Remove(coffeeQuantities[0]);
+                    milkQuantities.Pop();
+                    break;
+                case 100:
+                    cofeeDrinks["Capuccino"]++;
+                    coffeeQuantities.Remove(coffeeQuantities[0]);
+                    milkQuantities.Pop();
+                    break;
+                case 150:
+                    cofeeDrinks["Americano"]++;
+                    coffeeQuantities.Remove(coffeeQuantities[0]);
+                    milkQuantities.Pop();
+                    break;
+                case 200:
+                    cofeeDrinks["Latte"]++;
+                    if (!isOne)
+                    {
+                        coffeeQuantities.Remove(coffeeQuantities[0]);
+                    }
+                    milkQuantities.Pop();
+                    break;
+                default:
+                    defaultSum = true;
+                    if (coffeeQuantities.Count == 1)
+                    {
+                        sum += coffeeQuantities[0];
+                        isOne = true;
+                    }
+                    break;
+            }
         }
-        Console.WriteLine();
+        if (milkQuantities.Count == 0 && coffeeQuantities.Count == 0)
+        {
+            Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+            Console.WriteLine("Coffee left: none");
+            Console.WriteLine("Milk left: none");
+        }
+        else
+        {
+            Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+            if (coffeeQuantities.Count == 0)
+            {
+                Console.WriteLine("Coffee left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Coffee left: {string.Join(", ", coffeeQuantities)}");
+            }
+            if (milkQuantities.Count == 0)
+            {
+                Console.WriteLine("Milk left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Milk left: {string.Join(", ", milkQuantities)}");
+            }
+        }
+        var sortedDrinks = cofeeDrinks.OrderBy(x => x.Value).ThenByDescending(x => x.Key);
+        foreach (var cofee in sortedDrinks)
+        {
+            if (cofee.Value > 0)
+            {
+                Console.WriteLine($"{cofee.Key}: {cofee.Value}");
+            }
+        }
     }
 }
