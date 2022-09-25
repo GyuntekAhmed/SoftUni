61d00c41560a9620d45e2730e02ae5f3c16a73e8
Gyuntek <gyuntekahmed@abv.bg>
2022-09-25 23:33:46 +0300

    Initial commit

--- C# Advanced/Exercise-StreamsFilesAndDirectories/1.EvenLines/EvenLines.cs
+++ C# Advanced/Exercise-StreamsFilesAndDirectories/1.EvenLines/EvenLines.cs
@@ -1,1 +1,52 @@
-﻿
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            ProcessLines(inputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath)
+        {
+            using(StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int counter = -1;
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    counter++;
+
+                    if (counter % 2 == 0)
+                    {
+                        line = Replace(line);
+                        line = Reverse(line);
+                        Console.WriteLine(line);
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        public static string Reverse(string line)
+        {
+            return string.Join(" ", line.Split().Reverse());
+        }
+
+        public static string Replace(string line)
+        {
+            return line.Replace("-", "@")
+                .Replace(",", "@")
+                .Replace(".", "@")
+                .Replace("!", "@")
+                .Replace("?", "@");
+        }
+    }
+}
