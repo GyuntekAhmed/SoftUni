a76458906aa71bb447a511274f3dc9adc3c0cf4e
Gyuntek <gyuntekahmed@abv.bg>
2022-09-25 23:07:11 +0300

    Initial commit

--- C# Advanced/Exercise-StreamsFilesAndDirectories/1.EvenLines/EvenLines.cs
+++ C# Advanced/Exercise-StreamsFilesAndDirectories/1.EvenLines/EvenLines.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace _1.EvenLines
-{
-    internal class EvenLines
-    {
-        static void Main()
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
