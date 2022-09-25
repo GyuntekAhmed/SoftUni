10705d72823bdbdca1fb89399c44b9345b4c2df9
Gyuntek <gyuntekahmed@abv.bg>
2022-09-26 00:02:56 +0300

    Initial commit

---
+++ C# Advanced/Exercise-StreamsFilesAndDirectories/LineNumbers/LineNumbers.cs
@@ -0,0 +1,40 @@
+﻿
+namespace LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int count = 0;
+            List<string> outputLines = new List<string>();
+
+            foreach (var line in lines)
+            {
+                count++;
+
+                string midifiedLine = "Line " + count + ": " + line;
+
+                int countLetters = line.Count(char.IsLetter);
+                int countSymbol = line.Count(char.IsPunctuation);
+                string modifiedLine = $"Line {count}: {line} ({countLetters})({countSymbol})";
+                outputLines.Add(modifiedLine);
+            }
+
+            File.WriteAllLines(outputFilePath, outputLines);
+        }
+    }
+}
