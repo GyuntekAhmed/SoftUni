34a01cd2858ca5d75af044a0b5e1b8cc80b252cd
Gyuntek <gyuntekahmed@abv.bg>
2022-09-25 18:36:01 +0300

    Initial commit

---
+++ C# Advanced/StreamsFilesAndDirectories-Lab/03. Word Count/WordCount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Word_Count
+{
+    class WordCount
+    {
+        static void Main()
+        {
+
+            string wordPath = File.ReadAllText(@"..\..\..\Files\words.txt");
+            string[] words = wordPath.Split();
+            //using var writer = new StreamWriter(@"..\..\..\Files\output.txt");
+            //string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath, words);
+
+        }
+
+        public static void CalculateWordCounts(string wordPath, string textPath, string outputPath, string[] words)
+        {
+            var dictionary = new SortedDictionary<string, int>();
+
+            using (var reader = new StreamReader("text.txt"))
+            {
+                using (var writer = new StreamWriter(@"..\..\..\Files\output.txt"))
+                {
+
+
+                    string currentSentence = reader.ReadLine();
+
+                    while (currentSentence != null)
+                    {
+                        foreach (var word in words)
+                        {
+                            if (currentSentence.ToLower().Contains(word))
+                            {
+
+                                if (!dictionary.ContainsKey(word))
+                                {
+                                    dictionary.Add(word, 0);
+                                    dictionary[word]++;
+                                }
+                                else
+                                {
+                                    dictionary[word]++;
+                                }
+                            }
+                        }
+
+                        currentSentence = reader.ReadLine();
+                    }
+
+                    foreach (var word in dictionary.OrderByDescending(x => x.Value))
+                    {
+                        writer.WriteLine($"{word.Key} - {word.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/StreamsFilesAndDirectories-Lab/Line Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
