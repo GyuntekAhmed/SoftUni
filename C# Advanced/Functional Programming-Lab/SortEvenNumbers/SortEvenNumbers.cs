c9706762ccd4d6ecb586fdc739733120e5706098
Gyuntek <gyuntekahmed@abv.bg>
2022-09-26 22:29:04 +0300

    Initial commit

---
+++ C# Advanced/Functional Programming-Lab/SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+internal class SortEvenNumbers
+{
+    static void Main()
+    {
+        //int[] numbers = Console.ReadLine()
+        //    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+        //    .Select(n => int.Parse(n))
+        //    .Where(x => x % 2 == 0)
+        //    .OrderBy(n => n)
+        //    .ToArray();
+
+        Func<string, int> ParseStringToInt =
+            x => int.Parse(x);
+        Func<int, bool> isEven =
+            x => x % 2 == 0;
+        Func<int, int> identity =
+            x => x;
+        string input = Console.ReadLine();
+        string[] tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+        int[] nums = tokens.Select(ParseStringToInt).ToArray();
+        int[] evenNums = nums.Where(isEven).ToArray();
+        int[] orderedEvenNums = evenNums.OrderBy(identity).ToArray();
+
+        Console.WriteLine(string.Join(", ", orderedEvenNums));
+    }
+}
