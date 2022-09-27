7ebf722e639ce63f182ff598ada851c6152e95ed
Gyuntek <gyuntekahmed@abv.bg>
2022-09-27 23:40:44 +0300

    Initial commit

--- C# Advanced/FunctionalProgramming-Exercise/01. ActionPrint/ActionPrint.cs
+++ C# Advanced/FunctionalProgramming-Exercise/01. ActionPrint/ActionPrint.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 internal class ActionPrint
 {
     static void Main()
     {
-        
+        List<string> list = Console.ReadLine().Split().ToList();
+
+        Action<string> print = name => Console.WriteLine(name);
+
+        list.ForEach(print);
     }
 }
