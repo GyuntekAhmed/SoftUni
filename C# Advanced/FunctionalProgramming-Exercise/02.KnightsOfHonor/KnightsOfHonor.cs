778152b646652ce4a7c625b6f590cfc770f4af48
Gyuntek <gyuntekahmed@abv.bg>
2022-09-28 00:11:06 +0300

    Initial commit

---
+++ C# Advanced/FunctionalProgramming-Exercise/02.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class KnightsOfHonor
+{
+    static void Main()
+    {
+        List<string> list = Console.ReadLine().Split().ToList();
+
+        Action<string> print = name => Console.WriteLine($"Sir {name}");
+
+        list.ForEach(print);
+    }
+}
---
+++ C# Advanced/FunctionalProgramming-Exercise/03.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+
+class CustomMinFunction
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+        Func<List<int>, int> getMinElement = numbers => numbers.Min();
+
+        Console.WriteLine(getMinElement(numbers));
+    }
+}
