34ca9d80f85a0e47869065307f48b92b02d1b143
Gyuntek <gyuntekahmed@abv.bg>
2022-09-29 20:18:51 +0300

    Initial commit

--- C# Advanced/FunctionalProgramming-Exercise/08.ListOfPredicates/ListOfPredicates.cs
+++ C# Advanced/FunctionalProgramming-Exercise/08.ListOfPredicates/ListOfPredicates.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 internal class ListOfPredicates
 {
     static void Main()
     {
-        
+        List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+        int endRange = int.Parse(Console.ReadLine());
+
+        int[] dividers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int[] numbers = Enumerable.Range(1, endRange).ToArray();
+
+        foreach (int divider in dividers)
+        {
+            predicates.Add(p => p % divider == 0);
+        }
+
+        foreach (var number in numbers)
+        {
+            bool isDivisible = true;
+
+            foreach (var match in predicates)
+            {
+                if (!match(number))
+                {
+                    isDivisible = false;
+                    break;
+                }
+            }
+
+            if (isDivisible)
+            {
+                Console.Write($"{number} ");
+            }
+        }
     }
 }
