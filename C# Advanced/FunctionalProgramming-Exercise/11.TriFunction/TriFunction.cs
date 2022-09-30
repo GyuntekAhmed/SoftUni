030ba38912770a43d9fd5501eeaed784acf4c9f7
Gyuntek <gyuntekahmed@abv.bg>
2022-09-30 21:31:16 +0300

    Initial commit

---
+++ C# Advanced/FunctionalProgramming-Exercise/11.TriFunction/TriFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class TriFunction
+{
+    static void Main()
+    {
+        Func<string, int, bool> checkEqualOrLargerNameSum = (name, sum) =>
+            name.Sum(ch => ch) >= sum;
+
+        Func<string[], int, Func<string, int, bool>, string> getFirstName = (names, sum, match) =>
+            names.FirstOrDefault(name => match(name, sum));
+
+        int sum = int.Parse(Console.ReadLine());
+
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        Console.WriteLine(getFirstName(names, sum, checkEqualOrLargerNameSum));
+    }
+}
