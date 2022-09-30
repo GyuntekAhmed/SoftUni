f2d7741087c1753542adda69e204192a318c19a8
Gyuntek <gyuntekahmed@abv.bg>
2022-09-30 23:08:41 +0300

    Initial commit

---
+++ C# Advanced/DefiningClasses-Lab/CarManufacturer/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car()
+            {
+                Make = "Hyundai",
+                Model = "Santa Fe",
+                Year = 2009,
+                FuelConsumption = 10.5,
+                FuelQuantity = 75,
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Where to go ?");
+
+                car.Drive(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine($"Left fuel: {car.FuelQuantity}");
+                Console.WriteLine($"{car.WhoAmI()}");
+            }
+        }
+    }
+}
