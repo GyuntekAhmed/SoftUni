17864c47dc016057b9af2f4afac726f3d0c49cb4
Gyuntek <gyuntekahmed@abv.bg>
2022-10-01 20:51:36 +0300

    Initial commit

---
+++ C#Advanced/DefiningClasses-Exercise/DefiningClasses/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person person = new Person("Gyintek", 31);
+
+            Person person1 = new Person()
+            {
+                Name = "Gyuni",
+                Age = 7,
+            };
+            Person person2 = new Person();
+            person2.Name = "Georgi";
+            person2.Age = 31;
+
+            Console.WriteLine($"Name: {person.Name} Age: {person.Age}");
+            Console.WriteLine($"Name: {person1.Name} Age: {person1.Age}");
+            Console.WriteLine($"Name: {person2.Name} Age: {person2.Age}");
+        }
+    }
+}
