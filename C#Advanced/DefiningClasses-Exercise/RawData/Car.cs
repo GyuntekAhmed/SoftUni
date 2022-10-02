959fd92401b76b99a5e08617fd5e74d6122eebba
Gyuntek <gyuntekahmed@abv.bg>
2022-10-02 23:07:59 +0300

    Initial commit

---
+++ C#Advanced/DefiningClasses-Exercise/RawData/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace RawData
+{
+    public class Car
+    {
+        private Tire[] tires;
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+
+        public Tire[] Tires
+        {
+            get => tires;
+            set
+            {
+                if (value.Length == 4)
+                {
+                    tires = value;
+                }
+            }
+        }
+
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.Tires = tires;
+        }
+    }
+    public class Cargo
+    {
+        public string Type { get; set; }
+
+        public int Weight { get; set; }
+    }
+    public class Engine
+    {
+        public int Speed { get; set; }
+
+        public int Power { get; set; }
+    }
+    public class Tire
+    {
+        public double Pressure { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ C#Advanced/DefiningClasses-Exercise/RawData/RawData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    public class RawData
+    {
+        static void Main()
+        {
+            List<Car> cars = new List<Car>();
+
+            int countOfCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfCars; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', 6);
+
+                var cargo = new Cargo();
+                var engine = new Engine();
+                var tires = new List<Tire>();
+
+                string model = input[0];
+                engine.Speed = int.Parse(input[1]);
+                engine.Power = int.Parse(input[2]);
+                cargo.Weight = int.Parse(input[3]);
+                cargo.Type = input[4];
+                var spliter = input[5].Split().ToArray();
+
+                for (int j = 0; j < spliter.Length; j+= 2)
+                {
+                    var tire = new Tire();
+                    tire.Pressure = double.Parse(spliter[j]);
+                    tire.Age = int.Parse(spliter[j + 1]);
+
+                    tires.Add(tire);
+                }
+
+                var car = new Car(model, engine, cargo, tires.ToArray());
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                DisplayFragile(cars);
+            }
+            else
+            {
+                DisplayFlamable(cars);
+            }
+        }
+
+         static void DisplayFragile(List<Car> cars)
+        {
+            foreach (Car car in cars)
+            {
+                string model = string.Empty;
+
+                foreach (Tire tire in car.Tires)
+                {
+                    if (tire.Pressure < 1 && car.Model != model)
+                    {
+                        model = car.Model;
+                        Console.WriteLine(model);
+                    }
+                }
+            }
+        }
+
+        static void DisplayFlamable(List<Car> cars)
+        {
+            foreach (Car car in cars)
+            {
+                if (car.Engine.Power > 250)
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
+        }
+    }
+}
