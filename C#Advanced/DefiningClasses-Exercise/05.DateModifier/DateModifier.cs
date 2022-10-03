0022b387355c0a052bb5b612f8927b6cb73da6c4
Gyuntek <gyuntekahmed@abv.bg>
2022-10-03 22:32:51 +0300

    Initial commit

---
+++ C#Advanced/DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+        public static int DateDifference(string startDateAsString, string endDateAsString)
+        {
+            DateTime startDate = DateTime.Parse(startDateAsString);
+            DateTime endDate = DateTime.Parse(endDateAsString);
+
+            TimeSpan difference = endDate - startDate;
+
+            return Math.Abs(difference.Days);
+        }
+    }
+}
