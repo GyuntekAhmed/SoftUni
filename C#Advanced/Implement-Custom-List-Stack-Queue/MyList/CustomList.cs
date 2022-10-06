69f027488d5871eb510b9e9489baea7569f5db58
Gyuntek <gyuntekahmed@abv.bg>
2022-10-07 00:18:54 +0300

    Initial commit

--- C#Advanced/Implement-Custom-List-Stack-Queue/MyList/CustomList.cs
+++ C#Advanced/Implement-Custom-List-Stack-Queue/MyList/CustomList.cs
@@ -16,5 +16,92 @@ namespace MyList
         }
 
         public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException();
+                return items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException();
+                items[index] = value;
+            }
+        }
+
+        public void Add(int item)
+        {
+            if (Count == items.Length)
+            {
+                Resize();
+            }
+
+            items[Count] = item;
+
+            Count++;
+        }
+
+        public int RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new ArgumentOutOfRangeException();
+
+            int item = items[index];
+
+            items[index] = default(int);
+            ShiftToLeft(index);
+
+            Count--;
+
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+
+            return item;
+        }
+
+
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+
+        private void ShiftToLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+                items[i] = items[i + 1];
+        }
+
+        private void ShiftToRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                items[i] = items[i - 1];
+            }
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[items.Length / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
     }
 }
--- C#Advanced/Implement-Custom-List-Stack-Queue/MyList/StartUp.cs
+++ C#Advanced/Implement-Custom-List-Stack-Queue/MyList/StartUp.cs
@@ -6,7 +6,22 @@ namespace MyList
     {
         static void Main()
         {
-            
+            CustomList customList = new CustomList();
+
+            customList.Add(11);
+            customList.Add(12);
+            customList.Add(13);
+            customList.Add(14);
+            customList.Add(15);
+
+            customList.RemoveAt(0);
+
+            customList.RemoveAt(2);
+            customList.RemoveAt(0);
+
+            customList.Add(44);
+
+            Console.WriteLine(customList.Count);
         }
     }
 }
