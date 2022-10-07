ed8a5e4ec560f3c778255368bb66e5f6e16bb77d
Gyuntek <gyuntekahmed@abv.bg>
2022-10-07 18:52:00 +0300

    Initial commit

---
+++ C#Advanced/Generics-Lab/BoxOfT/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> elements;
+
+        public int Count => elements.Count;
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            var lastElement = elements[Count - 1];
+            elements.RemoveAt(Count - 1);
+
+            return lastElement;
+        }
+    }
+}
