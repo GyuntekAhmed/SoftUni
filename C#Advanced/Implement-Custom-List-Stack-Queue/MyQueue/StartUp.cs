cd9772c9408ec802952f07f3ea9fd4ba9701d8a9
Gyuntek <gyuntekahmed@abv.bg>
2022-10-07 18:15:02 +0300

    Initial commit

---
+++ C#Advanced/Implement-Custom-List-Stack-Queue/MyQueue/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyQueue
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            CustomQueue customQueue = new CustomQueue();
+
+            customQueue.Enqueue(10);
+            customQueue.Enqueue(20);
+            customQueue.Enqueue(30);
+            customQueue.Enqueue(40);
+            customQueue.Enqueue(50);
+
+            customQueue.Dequeue();
+
+            customQueue.Peek();
+
+            customQueue.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
