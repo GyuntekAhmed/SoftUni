6248fc15f9993b88eba0b30a921f845b5e85dcfd
Gyuntek <gyuntekahmed@abv.bg>
2022-10-08 22:36:49 +0300

    Initial commit

---
+++ C#Advanced/Generics-Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public EqualityScale()
+        {
+
+        }
+
+        public bool AreEqual(T left, T right)
+        {
+            return left.Equals(right);
+        }
+    }
+}
---
+++ C#Advanced/Generics-Lab/GenericScale/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            EqualityScale<int> nums = new EqualityScale<int>();
+
+            int num1 = 5;
+            int num2 = 6;
+
+            var result = nums.Equals(num1 == num2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
