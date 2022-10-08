2072ab5618f0d366779d0c8d7a63e12e1738081c
Gyuntek <gyuntekahmed@abv.bg>
2022-10-08 22:26:00 +0300

    Initial commit

--- C#Advanced/Generics-Lab/GenericArrayCreator/StartUp.cs
+++ C#Advanced/Generics-Lab/GenericArrayCreator/StartUp.cs
@@ -6,7 +6,11 @@ namespace GenericArrayCreator
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+
+            Console.WriteLine(string.Join(", ", strings));
+            Console.WriteLine(string.Join(", ", integers));
         }
     }
 }
