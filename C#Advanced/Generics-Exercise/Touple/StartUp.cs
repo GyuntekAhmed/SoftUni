e5dfbbdbf0ef4b3f84ee0b9240deba54c97ac7c5
Gyuntek <gyuntekahmed@abv.bg>
2022-10-09 22:25:10 +0300

    Initial commit

---
+++ C#Advanced/Generics-Exercise/Touple/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Touple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] personInfo = Console.ReadLine().Split();
+
+            string fullName = $"{personInfo[0]} {personInfo[1]}";
+            string city = $"{personInfo[2]}";
+
+            string[] nameAndBeer = Console.ReadLine().Split();
+            string name = nameAndBeer[0];
+            int numberOfLitters = int.Parse(nameAndBeer[1]);
+
+            string[] numbersInput = Console.ReadLine().Split();
+            int intNum = int.Parse(numbersInput[0]);
+            double doubleNum = double.Parse(numbersInput[1]);
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, city);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, numberOfLitters);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(intNum, doubleNum);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
+        }
+    }
+}
---
+++ C#Advanced/Generics-Exercise/Touple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Touple
+{
+    public class Tuple<TFirst, TSecond>
+    {
+        public Tuple(TFirst firstelement, TSecond secondelement)
+        {
+            FirstElement = firstelement;
+            Secondelement = secondelement;
+        }
+
+        public TFirst FirstElement { get; private set; }
+
+        public TSecond Secondelement { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstElement} -> {Secondelement}";
+        }
+    }
+}
