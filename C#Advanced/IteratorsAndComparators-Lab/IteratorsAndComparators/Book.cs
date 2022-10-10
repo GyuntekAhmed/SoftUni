7051365bfb9530da91c8abca949f81a891f57dac
Gyuntek <gyuntekahmed@abv.bg>
2022-10-10 22:53:21 +0300

    Initial commit

---
+++ C#Advanced/IteratorsAndComparators-Lab/IteratorsAndComparators/Book.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string[] autors)
+        {
+            Title = title;
+            Year = year;
+            Autors = autors.ToList();
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Autors { get; set; }
+    }
+}
