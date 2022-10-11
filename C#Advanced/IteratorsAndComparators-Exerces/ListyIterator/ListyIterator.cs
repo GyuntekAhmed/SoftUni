0250bce96be20eeb91c66a5db10389993de394f9
Gyuntek <gyuntekahmed@abv.bg>
2022-10-11 23:28:50 +0300

    Initial commit

---
+++ C#Advanced/IteratorsAndComparators-Exerces/ListyIterator/ListyIterator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> colection;
+        private int currentIndex;
+
+        public ListyIterator(params T[] data)
+        {
+            colection = new List<T>(data);
+            currentIndex = 0;
+        }
+
+        public bool HasNext() => currentIndex < colection.Count - 1;
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+
+            if (canMove)
+                currentIndex++;
+
+            return canMove;
+        }
+
+        public void Print()
+        {
+            if (colection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation");
+            }
+
+            Console.WriteLine($"{colection[currentIndex]}");
+        }
+    }
+}
---
+++ C#Advanced/IteratorsAndComparators-Exerces/ListyIterator/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            ListyIterator<string> listy = null;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split();
+
+                if (tokens[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(tokens.Skip(1).ToArray());
+                }
+                else if (tokens[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (tokens[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (tokens[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+            }
+        }
+    }
+}
