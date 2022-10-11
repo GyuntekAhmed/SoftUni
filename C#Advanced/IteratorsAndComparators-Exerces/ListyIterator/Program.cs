d4894ee8f8ef1d8b1a7fa3595cb5ca23e5678d08
Gyuntek <gyuntekahmed@abv.bg>
2022-10-11 22:58:30 +0300

    Initial commit

---
+++ C#Advanced/IteratorsAndComparators-Exerces/ListyIterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListyIterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
