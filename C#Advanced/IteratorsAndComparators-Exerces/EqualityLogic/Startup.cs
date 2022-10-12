09fb13ae63a551d3e0b52ef79a6eee2c077956f4
Gyuntek <gyuntekahmed@abv.bg>
2022-10-13 00:00:15 +0300

    Initial commit

---
+++ C#Advanced/IteratorsAndComparators-Exerces/EqualityLogic/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    internal class Startup
+    {
+        static void Main()
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hasSet = new HashSet<Person>();
+            var lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var person = new Person(tokens[0], int.Parse(tokens[1]));
+                sortedSet.Add(person);
+                hasSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hasSet.Count);
+        }
+    }
+}
