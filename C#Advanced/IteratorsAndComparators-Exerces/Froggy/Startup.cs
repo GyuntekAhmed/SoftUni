34a5f5d013766c862e886d3743c65b0bd8e9b6e9
Gyuntek <gyuntekahmed@abv.bg>
2022-10-12 23:34:28 +0300

    Initial commit

---
+++ C#Advanced/IteratorsAndComparators-Exerces/Froggy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            var lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
