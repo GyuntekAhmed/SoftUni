cf87bc5c893a59e653d2ff49b7d0500086b2367d
Gyuntek <gyuntekahmed@abv.bg>
2022-10-15 17:21:17 +0300

    Initial commit

---
+++ C#Advanced/Exams/01.MealPlan/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.MealPlan
+{
+    internal class Startup
+    {
+        static void Main()
+        {
+            Queue<string> meals = new Queue<string>(Console.ReadLine().Split());
+
+            Stack<int> dailyCalories = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList());
+
+        }
+    }
+}
