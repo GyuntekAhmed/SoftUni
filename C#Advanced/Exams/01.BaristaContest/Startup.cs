213c9c9132897139b72a4ec3c4665cb1e3588fae
Gyuntek <gyuntekahmed@abv.bg>
2022-10-17 23:31:13 +0300

    Initial commit

---
+++ C#Advanced/Exams/01.BaristaContest/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BaristaContest
+{
+    internal class Startup
+    {
+        static void Main()
+        {
+            Dictionary<string, int> drinksTable = new Dictionary<string, int>
+            {
+                {"Cortado", 50 },
+                {"Espresso", 75 },
+                {"Capuccino", 100 },
+                {"Americano", 150 },
+                {"Latte", 200 },
+            };
+            Dictionary<string, int> drinksCount = new Dictionary<string, int>();
+
+            Queue<int> cofeeQuantities = new Queue<int>(Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray());
+            Stack<int> milkQuantities = new Stack<int>(Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray());
+
+            while (cofeeQuantities.Count != 0 || milkQuantities.Count != 0)
+            {
+                int sumOfDrinks = cofeeQuantities.Peek() + milkQuantities.Peek();
+
+                foreach (var drink in drinksTable)
+                {
+                    if (sumOfDrinks == drink.Value)
+                    {
+                        string drinkName = drink.Key;
+                        if (!drinksCount.ContainsKey(drinkName))
+                        {
+                            drinksCount.Add(drinkName, 1);
+                            cofeeQuantities.Dequeue();
+                            milkQuantities.Pop();
+                            break;
+                        }
+                        else
+                        {
+                            drinksCount[drinkName]++;
+                            cofeeQuantities.Dequeue();
+                            milkQuantities.Pop();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
