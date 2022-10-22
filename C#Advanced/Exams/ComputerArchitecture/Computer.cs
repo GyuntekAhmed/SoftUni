282ff05a721f1a50ee293102fabb5cbf818d9e0d
Gyuntek <gyuntekahmed@abv.bg>
2022-10-22 13:19:18 +0300

    Initial commit

---
+++ C#Advanced/Exams/ComputerArchitecture/Computer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        private List<CPU> multiprocessor;
+
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new List<CPU>();
+        }
+
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public List<CPU> Multiprocessor { get; set; }
+        public int Count => Multiprocessor.Count;
+
+
+        public void Add(CPU cpu)
+        {
+            if (Capacity >= Count)
+            {
+                
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            foreach (var item in Multiprocessor)
+            {
+                if (item.Brand == brand)
+                {
+                    Multiprocessor.Remove(item);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public CPU MostPowerful()
+        {
+            var PowerFul = Multiprocessor.Max(x => x.Frequency);
+            var best = Multiprocessor.FirstOrDefault(x => x.Frequency == PowerFul);
+
+            return best;
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            foreach (var cpu in Multiprocessor)
+            {
+                if (cpu.Brand == brand)
+                {
+                    return cpu;
+                }
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"CPUs in the Computer {this.Model}:");
+
+            foreach (var cpu in Multiprocessor)
+            {
+                sb.AppendLine(cpu.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#Advanced/Exams/EnergyDrinks/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnergyDrinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> miligrams = new Stack<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            Queue<int> energyDrinks = new Queue<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int canDrink = 0;
+            int stamatCafeine = 300;
+
+            while (miligrams.Count != 0 && energyDrinks.Count != 0)
+            {
+                int sum = miligrams.Peek() * energyDrinks.Peek();
+
+                if (sum < stamatCafeine)
+                {
+                    stamatCafeine -= sum;
+                    miligrams.Pop();
+                    energyDrinks.Dequeue();
+                    canDrink += sum;
+                }
+                else
+                {
+                    miligrams.Pop();
+                    int currentDrink = energyDrinks.Peek();
+                    energyDrinks.Dequeue();
+                    energyDrinks.Enqueue(currentDrink);
+                    canDrink -= 30;
+                    stamatCafeine -= Math.Abs(canDrink);
+                    stamatCafeine = 300 - Math.Abs(canDrink);
+                }
+            }
+
+            if (energyDrinks.Count > 0)
+            {
+                Console.WriteLine($"Drinks left: {string.Join(", ", energyDrinks)}");
+            }
+            else
+            {
+                Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+            }
+            Console.WriteLine($"Stamat is going to sleep with {canDrink} mg caffeine.");
+        }
+    }
+}
