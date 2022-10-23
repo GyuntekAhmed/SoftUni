93b88455ad65b94415a72108960c591b69fe1b11
Gyuntek <gyuntekahmed@abv.bg>
2022-10-23 23:44:52 +0300

    Initial commit

--- Object-OrientedProgramming/Inheritance-Exercise/Zoo/StartUp.cs
+++ Object-OrientedProgramming/Inheritance-Exercise/Zoo/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zoo
 {
@@ -6,7 +7,30 @@ namespace Zoo
     {
         static void Main(string[] args)
         {
-            
+            List<object> animals = new List<object>();
+            List<string> names = new List<string>();
+
+            while (true)
+            {
+                string tokens = Console.ReadLine();
+                if (tokens == "end")
+                {
+                    break;
+                }
+                string name = tokens;
+                names.Add(name);
+
+            }
+
+            Bear bear = new Bear(names[0]);
+            animals.Add(bear);
+            Gorilla gorilla = new Gorilla(names[1]);
+            animals.Add(gorilla);
+            Snake snake = new Snake(names[2]);
+            animals.Add(snake);
+            Lizard lizard = new Lizard(names[3]);
+            animals.Add(lizard);
+
         }
     }
 }
