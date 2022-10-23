db68357362deb7e5289f7a8aacb49245cf286644
Gyuntek <gyuntekahmed@abv.bg>
2022-10-23 23:18:29 +0300

    Initial commit

---
+++ Object-OrientedProgramming/Inheritance-Exercise/Person/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+
+		private string name;
+		private int age;
+
+		public Person(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public virtual int Age
+		{
+			get { return age; }
+			set 
+			{
+				if (value < 0)
+				{
+					throw new Exception();
+				}
+				age = value; 
+			}
+		}
+
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(String.Format("Name: {0}, Age: {1}",
+				this.Name,
+				this.Age));
+			return sb.ToString();
+		}
+	}
+}
