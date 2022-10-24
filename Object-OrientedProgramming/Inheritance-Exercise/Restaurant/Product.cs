3e0fea22e5841bec2069ffa0342620872f545b24
Gyuntek <gyuntekahmed@abv.bg>
2022-10-24 22:16:28 +0300

    Initial commit

---
+++ Object-OrientedProgramming/Inheritance-Exercise/Restaurant/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string Name { get; set; }
+
+        public virtual decimal Price { get; set; }
+    }
+}
---
+++ Object-OrientedProgramming/Inheritance-Exercise/Restaurant/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Cake cake = new Cake("MyCake");
+
+            Console.WriteLine(cake.Name);
+            Console.WriteLine(cake.Price);
+        }
+    }
+}
