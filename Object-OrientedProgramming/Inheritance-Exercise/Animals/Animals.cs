9dfb89fb7d123d7b3e717776e36e31e6161d2575
Gyuntek <gyuntekahmed@abv.bg>
2022-10-25 01:14:40 +0300

    Initial commit

---
+++ Object-OrientedProgramming/Inheritance-Exercise/Animals/Animals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Animals
+{
+    public class Animals
+    {
+        public Animals(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Object-OrientedProgramming/Inheritance-Exercise/Animals/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+            while (true)
+            {
+                Animals animals = default;
+                string commands = Console.ReadLine();
+
+                if (commands == "Beast!")
+                {
+                    break;
+                }
+
+                string typeOfAnimal = commands;
+                string[] tokens = Console.ReadLine().Split();
+                string nameOfAnimal = tokens[0];
+                int ageOfAnimal = int.Parse(tokens[1]);
+
+                if (ageOfAnimal < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                switch (typeOfAnimal)
+                {
+                    case "Dog":
+                        animals = new Dog(nameOfAnimal, ageOfAnimal, tokens[2]);
+                        break;
+                    case "Cat":
+                        animals = new Cat(nameOfAnimal, ageOfAnimal, tokens[2]);
+                        break;
+                    case "Kitten":
+                        animals = new Kitten(nameOfAnimal, ageOfAnimal);
+                        break;
+                    case "Tomcat":
+                        animals = new Tomcat(nameOfAnimal, ageOfAnimal);
+                        break;
+                    case "Frog":
+                        animals = new Frog(nameOfAnimal, ageOfAnimal, tokens[2]);
+                        break;
+                }
+                Console.WriteLine(typeOfAnimal);
+                Console.WriteLine($"{animals.Name} {animals.Age} {animals.Gender}");
+                string sound = animals.ProduceSound();
+                Console.WriteLine(sound);
+            }
+        }
+    }
+}
