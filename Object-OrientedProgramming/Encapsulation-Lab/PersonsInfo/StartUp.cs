73f8c8b5aa2d76a34142aa93d833a0909e142174
Gyuntek <gyuntekahmed@abv.bg>
2022-10-27 23:16:56 +0300

    Initial commit

--- Object-OrientedProgramming/Encapsulation-Lab/PersonsInfo/StartUp.cs
+++ Object-OrientedProgramming/Encapsulation-Lab/PersonsInfo/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PersonsInfo
 {
@@ -6,7 +8,31 @@ namespace PersonsInfo
     {
         static void Main()
         {
-            
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+            }
+            //var parcentage = decimal.Parse(Console.ReadLine());
+            //persons.ForEach(p => p.IncreaseSalary(parcentage));
+            //persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
         }
     }
 }
