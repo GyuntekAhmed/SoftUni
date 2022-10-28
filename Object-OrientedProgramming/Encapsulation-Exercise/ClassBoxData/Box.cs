5ac0c922542ab0830e1885b767d73dbbb8cea737
Gyuntek <gyuntekahmed@abv.bg>
2022-10-28 18:01:55 +0300

    Initial commit

---
+++ Object-OrientedProgramming/Encapsulation-Exercise/ClassBoxData/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        //        •	Length - double, should not be zero or negative number
+        //•	Width - double, should not be zero or negative number
+        //•	Height - double, should not be zero or negative number
+        public double Length 
+        { 
+            get { return this.Length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{this.Length} cannot be zero or negative.");
+                }
+            }
+        }
+
+    }
+}
---
+++ Object-OrientedProgramming/Encapsulation-Exercise/ClassBoxData/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
