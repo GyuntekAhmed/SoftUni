db18139b8ba6e5ca3fe807c7dd3e6fb22ba98a96
Gyuntek <gyuntekahmed@abv.bg>
2022-10-30 01:42:20 +0300

    Initial commit

---
+++ Object-OrientedProgramming/Encapsulation-Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+		private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            this.products = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> Products => this.products.AsReadOnly();
+
+        public string Name
+		{
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return money; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            if (Money - product.Cost < 0)
+            {
+                return false;
+            }
+
+            this.products.Add(product);
+            this.Money -= product.Cost;
+
+            return true;
+        }
+
+    }
+}
---
+++ Object-OrientedProgramming/Encapsulation-Exercise/ShoppingSpree/StartUp.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] people = Console.ReadLine()
+                .Split(new char[] {';', '='}, StringSplitOptions.RemoveEmptyEntries);
+
+            string[] products = Console.ReadLine()
+                .Split(new char[] {';', '='}, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, Person> peopleKvp =
+                new Dictionary<string, Person>();
+            Dictionary<string, Product> productKvp =
+                new Dictionary<string, Product>();
+
+            try
+            {
+                for (int i = 0; i < people.Length; i += 2)
+                {
+                    string name = people[i];
+                    decimal money = decimal.Parse(people[i + 1]);
+
+                    Person person = new Person(name, money);
+                    peopleKvp.Add(name, person);
+                }
+
+                for (int i = 0; i < products.Length; i += 2)
+                {
+                    string name = products[i];
+                    decimal cost = decimal.Parse(products[i + 1]);
+
+                    Product product = new Product(name, cost);
+                    productKvp.Add(name, product);
+                }
+
+                while (true)
+                {
+                    string command = Console.ReadLine();
+
+                    if (command == "END")
+                    {
+                        break;
+                    }
+
+                    string[] tokens = command.Split();
+
+                    string personName = tokens[0];
+                    string productName = tokens[1];
+
+                    Person person = peopleKvp[personName];
+                    Product product = productKvp[productName];
+
+                    bool isAdd = person.AddProduct(product);
+
+                    if (!isAdd)
+                    {
+                        Console.WriteLine($"{personName} can't afford {productName}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{personName} bought {productName}");
+                    }
+                }
+
+                foreach (var peoples in peopleKvp)
+                {
+                    string info = peoples.Value.Products.Count > 0
+                        ? string.Join(", ", peoples.Value.Products.Select(x => x.Name))
+                        : "Nothing bought";
+
+                    Console.WriteLine($"{peoples.Key} - {info}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
