f3d5539f86cb94a107b720ced32adf32986b1c67
Gyuntek <gyuntekahmed@abv.bg>
2022-11-01 22:32:32 +0200

    Initial commit

---
+++ Object-OrientedProgramming/Encapsulation-Exercise/FootballTeamGenerator/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FootballTeamGenerator
+{
+    public class Player
+    {
+        private string name;
+
+        public Player(string name, Stats stats)
+        {
+            this.Name = name;
+            this.Stats = stats;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                //True -> null, '', '   '
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ErrorMessages.NullOrWhitespaceName);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public Stats Stats { get; private set; }
+
+        //5 integer properties
+    }
+}
