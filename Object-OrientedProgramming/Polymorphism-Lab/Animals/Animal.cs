bf5135d44be0ac10de5ef2fc9a073f0e62f1102a
Gyuntek <gyuntekahmed@abv.bg>
2022-11-05 21:47:01 +0200

    Upload excercise for Polymorphism - Lab solution

---
+++ Object-OrientedProgramming/Polymorphism-Lab/Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get ; private set ; }
+
+        public string FavouriteFood { get; private set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood}";
+        }
+    }
+}
