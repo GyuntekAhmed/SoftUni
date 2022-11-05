4d025e81f42dd78d016b230842993494847a9491
Gyuntek <gyuntekahmed@abv.bg>
2022-11-05 22:57:25 +0200

    Upload solution to Shapes of Polymorphism

---
+++ Object-OrientedProgramming/Polymorphism-Lab/Shapes/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override string Draw() => base.Draw() + GetType().Name;
+    }
+}
---
+++ Object-OrientedProgramming/Polymorphism-Lab/Shapes/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height { get; protected set; }
+        public double Width { get; protected set; }
+
+        public override double CalculateArea() => Height * Width;
+
+        public override double CalculatePerimeter() => (2 * Height) + (2 * Width);
+
+        public override string Draw() => base.Draw() + GetType().Name;
+    }
+}
