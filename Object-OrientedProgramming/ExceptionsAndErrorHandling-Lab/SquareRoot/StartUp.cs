20617f7b858590bc05dd2699c0b5e5b6c5aed3c9
Gyuntek <gyuntekahmed@abv.bg>
2022-11-11 00:23:10 +0200

    Added folder ExceptionsAndErrorHandling-Lab

---
+++ Object-OrientedProgramming/ExceptionsAndErrorHandling-Lab/SquareRoot/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArithmeticException("Invalid number.");
+                }
+                double sqr = Math.Sqrt(number);
+
+                Console.WriteLine(sqr);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
