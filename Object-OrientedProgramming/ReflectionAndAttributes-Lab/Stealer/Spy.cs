f511eea117eae1a4be6c2d3ff3e4d65f7ae4e0f0
Gyuntek <gyuntekahmed@abv.bg>
2022-11-12 23:06:04 +0200

    Initial commit

--- Object-OrientedProgramming/ReflectionAndAttributes-Lab/Stealer/Spy.cs
+++ Object-OrientedProgramming/ReflectionAndAttributes-Lab/Stealer/Spy.cs
@@ -8,15 +8,17 @@ namespace Stealer
     public class Spy
     {
         private StringBuilder sb;
+        private Type classType;
 
         public Spy()
         {
             sb = new StringBuilder();
+            classType = Type.GetType("Hacker");
         }
 
         public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
         {
-            Type classType = Type.GetType(investigatedClass);
+            classType = Type.GetType(investigatedClass);
 
             FieldInfo[] classFields = classType.GetFields
                 (BindingFlags.Public | BindingFlags.Instance |
@@ -29,6 +31,35 @@ namespace Stealer
             foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
             {
                 sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = Type.GetType(className);
+
+            FieldInfo[] classFields = classType.GetFields
+                (BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            MethodInfo[] classPublicMethods = classType.GetMethods
+                (BindingFlags.Public | BindingFlags.Instance);
+            MethodInfo[] classNonPublicMethods = classType.GetMethods
+                (BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (FieldInfo field in classFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (MethodInfo method in classNonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+
+            foreach (MethodInfo method in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
             }
 
             return sb.ToString().Trim();
--- Object-OrientedProgramming/ReflectionAndAttributes-Lab/Stealer/StartUp.cs
+++ Object-OrientedProgramming/ReflectionAndAttributes-Lab/Stealer/StartUp.cs
@@ -8,8 +8,8 @@ namespace Stealer
         {
             Spy spy = new Spy();
 
-            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
-
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
             Console.WriteLine(result);
         }
     }
