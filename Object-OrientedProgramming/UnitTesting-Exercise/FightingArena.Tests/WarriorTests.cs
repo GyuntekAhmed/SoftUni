824afb2162ac904f2754c75ba64a2e5d77f06aa4
Gyuntek <gyuntekahmed@abv.bg>
2022-11-21 23:02:25 +0200

    Modified WarriorTests.cs

--- Object-OrientedProgramming/UnitTesting-Exercise/FightingArena.Tests/WarriorTests.cs
+++ Object-OrientedProgramming/UnitTesting-Exercise/FightingArena.Tests/WarriorTests.cs
@@ -5,6 +5,17 @@ namespace FightingArena.Tests
     [TestFixture]
     public class WarriorTests
     {
+        [Test]
+        public void ConstructorShouldInitialWariorName()
+        {
+            // Arrange
+            string ExpectedName = "Georgi";
+            Warrior warrior = new Warrior(ExpectedName, 100, 100);
 
+            string actualName = warrior.Name;
+
+            // Assert
+            Assert.AreEqual(ExpectedName, actualName);
+        }
     }
 }
