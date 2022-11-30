2f6595a7dec6329dee827af45cf03cf4be7ef148
Gyuntek <gyuntekahmed@abv.bg>
2022-12-01 00:17:01 +0200

    Initial commit

---
+++ Object-OrientedProgramming/Exams/Formula1/Formula1/Models/FormulaOneCar.cs
@@ -0,0 +1,27 @@
+﻿using Formula1.Models.Contracts;
+
+namespace Formula1.Models
+{
+    public static class FormulaOneCar : IFormulaOneCar
+    {
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set 
+            {
+                
+            }
+        }
+
+        public int Horsepower => throw new System.NotImplementedException();
+
+        public double EngineDisplacement => throw new System.NotImplementedException();
+
+        public double RaceScoreCalculator(int laps)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
