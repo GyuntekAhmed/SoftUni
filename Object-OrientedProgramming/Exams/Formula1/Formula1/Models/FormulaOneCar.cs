61c57cfba1675ccac4d906e91c820744367218f5
Gyuntek <gyuntekahmed@abv.bg>
2022-12-01 23:30:55 +0200

    Initial commit

--- Object-OrientedProgramming/Exams/Formula1/Formula1/Models/FormulaOneCar.cs
+++ Object-OrientedProgramming/Exams/Formula1/Formula1/Models/FormulaOneCar.cs
@@ -1,27 +1,73 @@
-﻿using Formula1.Models.Contracts;
-
-namespace Formula1.Models
+﻿namespace Formula1.Models
 {
-    public static class FormulaOneCar : IFormulaOneCar
+    using System;
+    using Contracts;
+    using Utilities;
+
+    public abstract class FormulaOneCar : IFormulaOneCar
     {
         private string model;
+        private int horsePower;
+        private double engineDisplacement;
+
+        public FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            Model = model;
+            Horsepower = horsepower;
+            EngineDisplacement = engineDisplacement;
+        }
 
         public string Model
         {
             get { return model; }
-            set 
+            set
             {
-                
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException
+                        (string.Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+
+                model = value;
             }
         }
 
-        public int Horsepower => throw new System.NotImplementedException();
+        public int Horsepower
+        {
+            get { return horsePower; }
+            private set
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException
+                        (string.Format(ExceptionMessages.InvalidF1HorsePower, value));
+                }
 
-        public double EngineDisplacement => throw new System.NotImplementedException();
+                horsePower = value;
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get
+            {
+                return engineDisplacement;
+            }
+            private set
+            {
+                if (value < 1.6 || value > 2.00)
+                {
+                    throw new ArgumentException
+                        (string.Format(ExceptionMessages.InvalidF1EngineDisplacement, value));
+                }
+
+                engineDisplacement = value;
+            }
+        }
 
         public double RaceScoreCalculator(int laps)
         {
-            throw new System.NotImplementedException();
+            return engineDisplacement / horsePower * laps;
         }
     }
 }
---
+++ Object-OrientedProgramming/Exams/Formula1/Formula1/Models/Race.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    internal class Race
+    {
+    }
+}
