78525dca6b6846a66bafb687b4e57440f31dc688
Gyuntek <gyuntekahmed@abv.bg>
2022-12-07 23:16:44 +0200

    Added Exercise for OOP/Exams solution and initial commit

---
+++ Object-OrientedProgramming/Exams/PlanetWars/Models/MilitaryUnits/Entities/AnonymousImpactUnit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits.Entities
+{
+    internal class AnonymousImpactUnit
+    {
+    }
+}
---
+++ Object-OrientedProgramming/Exams/PlanetWars/Repositories/Entities/UnitRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Repositories.Entities
+{
+    internal class UnitRepository
+    {
+    }
+}
