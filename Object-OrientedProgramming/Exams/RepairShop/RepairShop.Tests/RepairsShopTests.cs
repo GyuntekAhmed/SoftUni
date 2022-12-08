d89937473f35fb7b315519c4e481027667d5b2b8
Gyuntek <gyuntekahmed@abv.bg>
2022-12-08 19:48:18 +0200

    Initial commit

--- Object-OrientedProgramming/Exams/RepairShop/RepairShop.Tests/RepairsShopTests.cs
+++ Object-OrientedProgramming/Exams/RepairShop/RepairShop.Tests/RepairsShopTests.cs
@@ -4,9 +4,31 @@ namespace RepairShop.Tests
 {
     public class Tests
     {
+        [TestFixture]
         public class RepairsShopTests
         {
-           
+            private Car car;
+            private Garage garage;
+
+            [SetUp]
+            public void SetUp()
+            {
+                car = new Car("Hyundai", 1835);
+                garage = new Garage("My Garage", 3);
+            }
+
+            [Test]
+            public void CarConstructorShouldSetCorrectly()
+            {
+                string expectedName = "Hyundai";
+                string actualName = car.CarModel;
+
+                int expectedNumber = 1835;
+                int actualNumber = car.NumberOfIssues;
+
+                Assert.AreEqual(expectedName, actualName);
+                Assert.AreEqual(expectedNumber, actualNumber);
+            }
         }
     }
 }
