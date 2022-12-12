fda8c23c96d12c79a9b5d8046e4853f8f6afaf9a
Gyuntek <gyuntekahmed@abv.bg>
2022-12-12 19:24:24 +0200

    Initial commit

---
+++ Object-OrientedProgramming/Demos/Demos/CustomStack/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomStack
+{
+    using System.Collections.Generic;
+
+    public class StackOfStrings : Stack<string>
+    {
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> elements)
+        {
+            foreach (var element in elements)
+            {
+                Push(element);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ Object-OrientedProgramming/Demos/Demos/CustomStack/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomStack
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            StackOfStrings strings = new StackOfStrings();
+
+            Console.WriteLine(strings.IsEmpty());
+
+            strings.AddRange(new List<string>() { "11", "55", "44" });
+            Console.WriteLine(strings.IsEmpty());
+
+            Console.WriteLine(strings.Pop());
+            Console.WriteLine(strings.Pop());
+            Console.WriteLine(strings.Pop());
+        }
+    }
+}
