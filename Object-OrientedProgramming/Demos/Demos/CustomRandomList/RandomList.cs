14b6c889ba65f5d672a3cfab0308d02e09b59e1e
Gyuntek <gyuntekahmed@abv.bg>
2022-12-12 19:09:40 +0200

    Initial commit

---
+++ Object-OrientedProgramming/Demos/Demos/CustomRandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        Random random = new Random();
+
+        public string RandomString()
+        {
+            if (Count == 0)
+            {
+                return null;
+            }
+
+            var index = random.Next(0, Count);
+            var element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
---
+++ Object-OrientedProgramming/Demos/Demos/CustomRandomList/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomRandomList
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            RandomList list = new RandomList();
+
+            list.Add("Georgi");
+            list.Add("Jack");
+            list.Add("Anna");
+            list.Add("Eva");
+            list.Add("Brath");
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
