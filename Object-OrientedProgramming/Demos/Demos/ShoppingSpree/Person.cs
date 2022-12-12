3db16193d5721fd6fbdc7e3f7736492fa4cb67e9
Gyuntek <gyuntekahmed@abv.bg>
2022-12-12 21:13:58 +0200

    Initial commit

---
+++ Object-OrientedProgramming/Demos/Demos/ShoppingSpree/Person.cs
@@ -0,0 +1,65 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Person
+    {
+        private string name;
+        private double money;
+
+        private List<Product> products;
+
+        public Person(string name, double money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> Products => products;
+
+        public double Money
+        {
+            get { return money; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+
+                }
+
+                money = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            if (Money - product.Cost < 0)
+            {
+                return false;
+            }
+
+            products.Add(product);
+            Money -= product.Cost;
+            return true;
+        }
+    }
+}
---
+++ Object-OrientedProgramming/Demos/Demos/ShoppingSpree/Product.cs
@@ -0,0 +1,47 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+
+    public class Product
+    {
+        private string name;
+        private double cost;
+
+        public Product(string productName, double cost)
+        {
+            Cost = cost;
+            Name = productName;
+        }
+
+        public double Cost
+        {
+            get { return cost; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                cost = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+    }
+}
