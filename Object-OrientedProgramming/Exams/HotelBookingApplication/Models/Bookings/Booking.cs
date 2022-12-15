f4f6c5c68426b3462cd4b8ba0aa5ad727b957b37
Gyuntek <gyuntekahmed@abv.bg>
2022-12-15 19:54:57 +0200

    Added exercise for OOP/Exams solutions

---
+++ Object-OrientedProgramming/Exams/HotelBookingApplication/Models/Bookings/Booking.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Bookings
+{
+    internal class Booking
+    {
+    }
+}
---
+++ Object-OrientedProgramming/Exams/HotelBookingApplication/Models/Rooms/Entities/Studio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms.Entities
+{
+    internal class Studio
+    {
+    }
+}
