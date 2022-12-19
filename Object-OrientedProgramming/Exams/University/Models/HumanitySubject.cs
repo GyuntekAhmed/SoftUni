38368f3164d067d1700379da14381061dff106c8
Gyuntek <gyuntekahmed@abv.bg>
2022-12-19 20:31:19 +0200

    Added exercise for OOP/Exams solutions

---
+++ Object-OrientedProgramming/Exams/University/Models/HumanitySubject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityCompetition.Models
+{
+    internal class HumanitySubject
+    {
+    }
+}
---
+++ Object-OrientedProgramming/Exams/University/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace UniversityCompetition.Models
+{
+    using System.Collections.Generic;
+
+    using Contracts;
+
+    public class Student : IStudent
+    {
+        public int Id { get; set; }
+
+        public string FirstName => throw new System.NotImplementedException();
+
+        public string LastName => throw new System.NotImplementedException();
+
+        public IReadOnlyCollection<int> CoveredExams => throw new System.NotImplementedException();
+
+        public IUniversity University => throw new System.NotImplementedException();
+
+        public void CoverExam(ISubject subject)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void JoinUniversity(IUniversity university)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
