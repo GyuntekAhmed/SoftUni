7c76d3bcceb48864e61e860a1bbfb84d3730c583
Gyuntek <gyuntekahmed@abv.bg>
2022-12-24 02:02:19 +0200

    Starting MS-SQL course

---
+++ MS-SQL/Expenses/Expenses/Expenses.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Description] NCHAR(500) NOT NULL, 
+    [Amount] DECIMAL(18, 2) NOT NULL, 
+    [TimeOfExpense] SMALLDATETIME NULL
+)
--- Object-OrientedProgramming/Exams/University/Repositories/StudentRepository.cs
+++ Object-OrientedProgramming/Exams/University/Repositories/StudentRepository.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace UniversityCompetition.Repositories
+﻿namespace UniversityCompetition.Repositories
 {
-    internal class StudentRepository
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Models.Contracts;
+
+    public class StudentRepository : IRepository<IStudent>
     {
+        private List<IStudent> students;
+
+        public StudentRepository()
+        {
+            this.students = new List<IStudent>();
+        }
+
+        public IReadOnlyCollection<IStudent> Models => this.students;
+
+        public void AddModel(IStudent model)
+        {
+            this.students.Add(model);
+        }
+
+        public IStudent FindById(int id) => this.students.FirstOrDefault(s => s.Id == id);
+
+        public IStudent FindByName(string name)
+        {
+            string firstName = name.Split(" ")[0];
+            string lastName = name.Split(" ")[1];
+
+            return this.students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+        }
+
     }
 }
