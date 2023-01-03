6daa958405e4375454e922596562d2483b52e0bd
Gyuntek <gyuntekahmed@abv.bg>
2023-01-03 22:19:12 +0200

    Initial commit

--- MS-SQL/Subqueries and Joins/Subqueries and Joins/SQLQuery1.sql
+++ MS-SQL/Subqueries and Joins/Subqueries and Joins/SQLQuery1.sql
@@ -2,3 +2,15 @@ SELECT TOP(5) EmployeeID, JobTitle, a.AddressID, AddressText
 	FROM Employees AS e
   JOIN Addresses AS a ON A.AddressID = E.AddressID
     ORDER BY a.AddressID
+
+SELECT TOP(50) e.FirstName, e.LastName, t.Name, a.AddressText
+	FROM Employees AS e
+  JOIN Addresses AS a ON a.AddressID = e.AddressID
+  JOIN Towns AS t ON t.TownID = a.TownID
+	ORDER BY e.FirstName, e.LastName
+
+SELECT e.EmployeeID, e.FirstName, e.LastName, d.Name
+	FROM Employees AS e
+  JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
+	WHERE d.Name = 'Sales'
+	ORDER BY e.EmployeeID
