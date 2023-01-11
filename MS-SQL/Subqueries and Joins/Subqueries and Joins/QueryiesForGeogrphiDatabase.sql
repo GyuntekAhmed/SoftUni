6b439b97c88d7ae5320b3e2396e7cd814aa00c2d
Gyuntek <gyuntekahmed@abv.bg>
2023-01-11 21:59:29 +0200

    Initial commit

--- MS-SQL/Subqueries and Joins/Subqueries and Joins/QueryiesForGeogrphiDatabase.sql
+++ MS-SQL/Subqueries and Joins/Subqueries and Joins/QueryiesForGeogrphiDatabase.sql
@@ -22,3 +22,23 @@ SELECT TOP(5) c.CountryName, r.RiverName
 	ON r.Id = cr.RiverId
 	WHERE c.ContinentCode = 'AF'
 	ORDER BY c.CountryName
+
+SELECT ContinentCode, CurrencyCode, CurrencyUsage
+	FROM
+	(
+		SELECT *,
+			DENSE_RANK() OVER(PARTITION BY ContinentCode ORDER BY CurrencyUsage DESC)
+			AS CurrencyRank
+			FROM
+			(
+				SELECT cnt.ContinentCode, c.CurrencyCode,
+				 COUNT(c.CurrencyCode) AS CurrencyUsage
+					FROM Continents AS cnt
+				  LEFT JOIN Countries AS c
+					ON c.ContinentCode = cnt.ContinentCode
+					GROUP BY cnt.ContinentCode, c.CurrencyCode
+			) AS CurrencyQuery
+		WHERE CurrencyUsage > 1
+	) AS CurrencyRankingQuery
+	WHERE CurrencyRank = 1
+  ORDER BY ContinentCode
