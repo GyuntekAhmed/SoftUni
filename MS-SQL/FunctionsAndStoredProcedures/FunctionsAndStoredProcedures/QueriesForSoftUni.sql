2d836c57a1dcaee3127cc9cd3d7e00e7b31217a8
Gyuntek <gyuntekahmed@abv.bg>
2023-01-25 19:46:00 +0200

    Initial commit

--- MS-SQL/FunctionsAndStoredProcedures/FunctionsAndStoredProcedures/QueriesForSoftUni.sql
+++ MS-SQL/FunctionsAndStoredProcedures/FunctionsAndStoredProcedures/QueriesForSoftUni.sql
@@ -73,3 +73,27 @@ END
 GO
 
 EXEC [dbo].[usp_EmployeesBySalaryLevel] 'Low'
+GO
+
+CREATE FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR(50), @word VARCHAR(50))
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @currentIndex int = 1;
+
+WHILE(@currentIndex <= LEN(@word))
+	BEGIN
+
+	DECLARE @currentLetter varchar(1) = SUBSTRING(@word, @currentIndex, 1);
+
+	IF(CHARINDEX(@currentLetter, @setOfLetters)) = 0
+	BEGIN
+	RETURN 0;
+	END
+
+	SET @currentIndex += 1;
+	END
+
+RETURN 1;
+	
+END
