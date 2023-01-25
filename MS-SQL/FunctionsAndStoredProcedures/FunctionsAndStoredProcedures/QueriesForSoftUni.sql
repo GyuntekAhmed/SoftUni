7b2f281441a40d860a5edd47c38d2fc0b24b8469
Gyuntek <gyuntekahmed@abv.bg>
2023-01-25 20:11:37 +0200

    Initial commit

--- MS-SQL/FunctionsAndStoredProcedures/FunctionsAndStoredProcedures/QueriesForSoftUni.sql
+++ MS-SQL/FunctionsAndStoredProcedures/FunctionsAndStoredProcedures/QueriesForSoftUni.sql
@@ -97,3 +97,84 @@ WHILE(@currentIndex <= LEN(@word))
 RETURN 1;
 	
 END
+
+GO
+
+CREATE PROC usp_DeleteEmployeesFromDepartment (@departmentId INT)
+AS
+
+DECLARE @empIDsToBeDeleted TABLE
+(
+Id int
+)
+
+INSERT INTO @empIDsToBeDeleted
+SELECT e.EmployeeID
+  FROM Employees AS e
+ WHERE e.DepartmentID = @departmentId
+
+ALTER TABLE Departments
+ALTER COLUMN ManagerID int NULL
+
+DELETE FROM EmployeesProjects
+	  WHERE EmployeeID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+UPDATE Employees
+   SET ManagerID = NULL
+ WHERE ManagerID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+UPDATE Departments
+   SET ManagerID = NULL
+ WHERE ManagerID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+DELETE FROM Employees
+	  WHERE EmployeeID IN (SELECT Id FROM @empIDsToBeDeleted)
+
+DELETE FROM Departments
+	  WHERE DepartmentID = @departmentId 
+
+  SELECT COUNT(*) AS [Employees Count] FROM Employees AS e
+	JOIN Departments AS d
+	  ON d.DepartmentID = e.DepartmentID
+   WHERE e.DepartmentID = @departmentId
+
+GO
+
+CREATE FUNCTION ufn_CalculateFutureValue(@Sum DECIMAL(15,4),
+@YearlyInterestRate FLOAT,
+@NumberOfYears INT )
+RETURNS DECIMAL(15,4)
+BEGIN
+    DECLARE @FutureValue DECIMAL(15,4);
+
+    SET @FutureValue = @Sum * POWER((1 + @YearlyInterestRate), @NumberOfYears)
+    
+    RETURN @FutureValue
+END
+
+GO
+
+CREATE PROC usp_CalculateFutureValueForAccount (@AccountId INT, @InterestRate FLOAT) AS
+SELECT a.Id AS [Account Id],
+	   ah.FirstName AS [First Name],
+	   ah.LastName AS [Last Name],
+	   a.Balance,
+	   dbo.ufn_CalculateFutureValue(Balance, @InterestRate, 5) AS [Balance in 5 years]
+  FROM AccountHolders AS ah
+  JOIN Accounts AS a ON ah.Id = a.Id
+ WHERE a.Id = @AccountId
+
+ GO
+
+ CREATE FUNCTION ufn_CashInUsersGames(@Game VARCHAR(50))
+RETURNS TABLE
+AS
+RETURN
+SELECT SUM([k].[Cash]) AS [SumCash]
+  FROM (
+SELECT [ug].[Cash] AS [Cash],
+       ROW_NUMBER() OVER (PARTITION BY [g].[Name] ORDER BY [ug].[Cash] DESC) AS [Row]
+  FROM [dbo].[Games] AS g
+  JOIN [dbo].[UsersGames] AS [ug] ON [g].[Id] = [ug].[GameId]
+ WHERE [g].[Name] = @Game) AS k
+ WHERE [k].[Row] % 2 = 1
