559bb0089458544ae5d573fd63d2cf8359c3758d
Gyuntek <gyuntekahmed@abv.bg>
2023-02-01 00:14:40 +0200

    Initial commit

--- MS-SQL/Exams/Exams/'31.01.23'ExamPrep.sql
+++ MS-SQL/Exams/Exams/'31.01.23'ExamPrep.sql
@@ -73,3 +73,76 @@ CREATE TABLE [PartsNeeded]
 	PRIMARY KEY([JobId], [PartId]),
 	[Quantity] INT DEFAULT 1 CHECK([Quantity] > 0) NOT NULL
 )
+
+INSERT INTO [Clients] VALUES
+('Teri', 'Ennaco','570-889-5187'),
+('Merlyn', 'Lawler', '201-588-7810'),
+('Georgene', 'Montezuma', '925-615-5185'),
+('Jettie', 'Mconnell', '908-802-3564'),
+('Lemuel', 'Latzke', '631-748-6479'),
+('Melodie', 'Knipp', '805-690-1682'),
+('Candida', 'Corbley', '908-275-8357')
+
+INSERT INTO [Parts]
+([SerialNumber],
+ [Description],
+ [Price],
+ [VendorId]) VALUES
+('WP8182119', 'Door Boot Seal', 117.86, 2),
+('W10780048', 'Suspension Rod', 42.81, 1),
+('W10841140', 'Silicone Adhesive ', 6.77, 4),
+('WPY055980', 'High Temperature Adhesive', 13.94, 3)
+
+SELECT *
+  FROM [Jobs]
+ WHERE [MechanicId] = 3
+
+ UPDATE [Jobs]
+	SET [MechanicId] = 3, [Status] = 'In Progress'
+  WHERE [Status] = 'Pending'
+
+SELECT *
+  FROM [OrderParts]
+ WHERE [OrderId] = 19
+
+DELETE
+  FROM [OrderParts]
+ WHERE [OrderId] = 19
+
+DELETE
+  FROM [Orders]
+ WHERE [OrderId] = 19
+
+   SELECT CONCAT([m].FirstName, ' ', [m].LastName) AS [Mechanic],
+		  [j].[Status],
+		  [j].[IssueDate]
+     FROM [Mechanics]
+	   AS [m]
+LEFT JOIN [Jobs]
+	   AS [j]
+	   ON [m].[MechanicId] = [j].[MechanicId]
+ ORDER BY [m].[MechanicId],
+		  [j].[IssueDate],
+		  [j].[JobId]
+
+	SELECT CONCAT([c].[FirstName], ' ', [c].[LastName]) AS [Client],
+		   DATEDIFF(DAY, [j].IssueDate, '04/24/2017') AS [Days going],
+		   [j].[Status]
+	  FROM [Clients]
+		AS [c]
+	  JOIN [Jobs]
+		AS [j]
+		ON [c].[ClientId] = [j].[ClientId]
+	 WHERE [j].[Status] != 'Finished'
+  ORDER BY [Days going] DESC,
+		   [c].[ClientId]
+
+	SELECT CONCAT([m].[FirstName], ' ', [m].[LastName]) AS [Mechanic],
+		   AVG(DATEDIFF(DAY, [j].IssueDate, [j].[FinishDate])) AS [Average Days]
+	  FROM [Mechanics]
+	    AS [m]
+	  JOIN [Jobs]
+	    AS [j]
+		ON [m].[MechanicId] = [j].[MechanicId]
+  GROUP BY [m].[FirstName], [m].[LastName], [m].[MechanicId]
+  ORDER BY [m].[MechanicId]
