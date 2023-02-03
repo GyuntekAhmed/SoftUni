9b3343478826e8fc2198a746502f8b417a3737a8
Gyuntek <gyuntekahmed@abv.bg>
2023-02-03 23:19:36 +0200

    Initial commit

--- MS-SQL/Exams/Exams/'31.01.23'ExamPrep.sql
+++ MS-SQL/Exams/Exams/'31.01.23'ExamPrep.sql
@@ -204,3 +204,81 @@ LEFT JOIN [Orders]
     WHERE [j].[Status] != 'Finished' AND
 		  [p].[StockQty] + IIF([o].[Delivered] = 0, [op].[Quantity], 0) < [pn].[Quantity]
  ORDER BY [p].[PartId]
+
+ GO
+
+ CREATE PROC usp_PlaceOrder (@jobId INT, @serialNumber VARCHAR(50), @quantity INT)
+ AS
+ BEGIN
+	DECLARE @status VARCHAR(10) =
+	(
+		SELECT [Status]
+		  FROM [Jobs]
+		 WHERE [JobId] = @jobId
+	)
+	DECLARE @partId VARCHAR(10) =
+	(
+		SELECT [PartId]
+		  FROM [Parts]
+		 WHERE [SerialNumber] = @serialNumber
+	)
+IF (@quantity <= 0)
+ THROW 50012, 'Part quantity must be more than zero!', 1
+ELSE IF (@status IS NULL)
+ THROW 50013, 'Job not found!', 1
+ELSE IF (@status = 'Finished')
+ THROW 50011, 'This job is not active!', 1
+ELSE IF (@partId IS NULL)
+ THROW 50014, 'Part not found!', 1
+
+   DECLARE @orderId INT =
+	(
+		SELECT [o].[OrderId]
+		  FROM [Orders]
+		    AS [o]
+		  JOIN [OrderParts]
+		    AS [op]
+			ON [o].[OrderId] = [op].[OrderId]
+		  JOIN [Parts]
+		    AS [p]
+			ON [op].[PartId] = [p].[PartId]
+		 WHERE [JobId] = @jobId AND
+			   [p].[PartId] = @partId
+	)
+	
+IF (@orderId IS NULL)
+BEGIN
+	INSERT INTO [Orders] ([JobId], [IssueDate]) VALUES
+	(@jobId, NULL)
+
+	SET @orderId =
+		(
+			SELECT [OrderId]
+			  FROM [Orders]
+			 WHERE [JobId] = @jobId AND
+				   [IssueDate] IS NULL
+		)
+	INSERT INTO [OrderParts] ([OrderId], [PartId], [Quantity]) VALUES
+	(@orderId, @partId, @quantity)
+END
+ELSE
+	BEGIN
+	DECLARE @issueDate DATE = 
+	 (
+		SELECT [IssueDate]
+		  FROM [Orders]
+		 WHERE [OrderId] = @orderId AND
+			   [JobId] = @jobId
+	 )
+	 IF (@issueDate IS NULL)
+		INSERT INTO [OrderParts]([OrderId], [PartId], [Quantity]) VALUES
+		(@orderId, @partId, @quantity)
+	 ELSE
+		UPDATE [OrderParts]
+		   SET [Quantity] += [Quantity]
+		 WHERE [OrderId] = @orderId AND
+			   [PartId] = @partId
+	END
+ END
+
+GO
