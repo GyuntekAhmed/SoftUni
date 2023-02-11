1f6934975747a6602c109755e033e946cb5e2d14
Gyuntek <gyuntekahmed@abv.bg>
2023-02-11 20:28:40 +0200

    Initial commit

--- Entity-Framework-Core/CSharpDbDemo/Program.cs
+++ Entity-Framework-Core/CSharpDbDemo/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System.Data.SqlClient;
 
-//string connectionString = "Server=.\\SQLEXPRESS01;User Id=Gyuni;Password=...;Database=SoftUni2";
+//string connectionString = "Server=.\\SQLEXPRESS01;User Id=Gyuni;Password=...;Database=SoftUni";
 string connectionString = "Server=.\\SQLEXPRESS01;Integrated Security=true;Database=SoftUni";
 using (var connection = new SqlConnection(connectionString))
 {
     connection.Open();
 
-    string query = "UPDATE Employees SET Salary -= 0.12";
+    string query = "SELECT * FROM Employees";
 
     SqlCommand cmd = new SqlCommand(query, connection);
 
-    var rowAffected = cmd.ExecuteNonQuery();
-
-    Console.WriteLine(rowAffected);
+    using (SqlDataReader reader = cmd.ExecuteReader())
+    {
+        while(reader.Read())
+        {
+           Console.WriteLine(reader[1]);
+        }
+    }
 }
