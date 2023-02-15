0a4437028edc2d564bcacc9ef94edf1b73881093
Gyuntek <gyuntekahmed@abv.bg>
2023-02-15 07:30:13 +0200

    Initial commit

--- MS-SQL/Exams/Exams/SQLQuery1.sql
+++ MS-SQL/Exams/Exams/SQLQuery1.sql
@@ -32,7 +32,7 @@ CREATE TABLE [Feedbacks]
 (
 	[Id] INT PRIMARY KEY IDENTITY,
 	[Description] VARCHAR(255),
-	[Rate] FLOAT CHECK([Rate] >= 0.00 AND [Rate] <= 10.00),
+	[Rate] DECIMAL(18,2) CHECK([Rate] >= 0.00 AND [Rate] <= 10.00),
 	[ProductId] INT FOREIGN KEY REFERENCES [Products](Id),
 	[CustomerId] INT FOREIGN KEY REFERENCES [Customers](Id)
 )
@@ -59,4 +59,36 @@ CREATE TABLE [ProductsIngredients]
 (
 	[ProductId] INT FOREIGN KEY REFERENCES [Products](Id),
 	[IngredientId] INT FOREIGN KEY REFERENCES [Ingredients](Id)
+	PRIMARY KEY ([ProductId], [IngredientId])
 )
+
+GO
+
+INSERT INTO [Distributors] 
+		   ([Name],
+			[CountryId], 
+			[AddressText],
+			[Summary])
+VALUES
+('Deloitte & Touche', 2,' 6 Arch St #9757', 'Customizable neutral traveling'),
+('Congress Title', 13, '58 Hancock St', 'Customer loyalty'),
+('Kitchen People', 1, '3 E 31st St #77', 'Triple-buffered stable delivery'),
+('General Color Co Inc', 21, '6185 Bohn St #72', 'Focus group'),
+('Beck Corporation', 23, '21 E 64th Ave', 'Quality-focused 4th generation hardware')
+
+INSERT INTO [Customers]
+		   ([FirstName],
+		    [LastName],
+			[Age], 
+			[Gender], 
+			[PhoneNumber],
+			[CountryId])
+VALUES
+('Francoise', 'Rautenstrauch', 15, 'M', '0195698399', 5),
+('Kendra', 'Loud', 22, 'F', '0063631526', 11),
+('Lourdes', 'Bauswell', 50, 'M', '0139037043', 8),
+('Hannah', 'Edmison', 18, 'F', '0043343686', 1),
+('Tom', 'Loeza', 31, 'M', '0144876096', 23),
+('Queenie', 'Kramarczyk', 30, 'F', '0064215793', 29),
+('Hiu', 'Portaro', 25, 'M', '0068277755', 16),
+('Josefa', 'Opitz', 43, 'F', '0197887645', 17)
