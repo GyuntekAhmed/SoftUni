b9364ad5a95335f5f7049437f430aa76ebb0fb36
gyuntekahmed <gyuntekahmed@abv.bg>
2023-02-23 22:34:51 +0200

    Initial commit

---
+++ Entity-Framework-Core/EntityFrameworkCore-Exercise/Models/Project.cs
@@ -0,0 +1,24 @@
+﻿namespace SoftUni.Models
+{
+    using System.Collections.Generic;
+
+    public class Project
+    {
+        public Project()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int ProjectId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
