5f029a96527f2f79a930fedb331404cdd52e5852
gyuntekahmed <gyuntekahmed@abv.bg>
2023-03-18 19:54:29 +0200

    Adder project ProductShop for XML-Procesing folder

---
+++ Entity-Framework-Core/XML-Processing/01. Import Users_Skeleton (ProductShop)/ProductShop/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+
+        private static IMapper InitializeAutoMapper()
+            => new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+    }
+}
