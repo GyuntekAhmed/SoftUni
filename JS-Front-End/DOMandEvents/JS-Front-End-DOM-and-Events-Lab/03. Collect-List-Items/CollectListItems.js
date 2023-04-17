a1960bc05e45cecf6fd53bb0f25a749eb3852049
gyuntekahmed <gyuntekahmed@abv.bg>
2023-04-17 12:43:48 +0300

    Initial commit

--- JS-Front-End/DOMandEvents/JS-Front-End-DOM-and-Events-Lab/03. Collect-List-Items/CollectListItems.js
+++ JS-Front-End/DOMandEvents/JS-Front-End-DOM-and-Events-Lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,6 @@
 function extractText() {
-    // TODO
+    const ancor = document.getElementById(`more`);
+    const span = document.getElementById(`text`);
+    span.style.display = `inline`;
+    ancor.style.display = `none`;
 }
