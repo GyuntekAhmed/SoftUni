b6a96ee29674f860af438a235d0f91af9f38f773
gyuntekahmed <gyuntekahmed@abv.bg>
2023-05-09 19:45:47 +0300

    Add Andrey's shop project and initial commit

---
+++ ASP.NET-Fundamentals/Andrey's-Workshop/Andreys/Data/AndreysDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace Andreys.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+
+    public class AndreysDbContext : DbContext
+    {
+        public AndreysDbContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.
+                UseSqlServer("Server=.;Database=Andreys;Integrated Security=True;");
+        }
+    }
+}
---
+++ ASP.NET-Fundamentals/Andrey's-Workshop/Andreys/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Andreys.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public Category Category { get; set; }
+
+        public Gender Gender { get; set; }
+    }
+}
