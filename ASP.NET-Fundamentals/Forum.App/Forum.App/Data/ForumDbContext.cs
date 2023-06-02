bd02185aa71e2fdffb26a7bac298dba3bdb5b9f6
gyuntekahmed <gyuntekahmed@abv.bg>
2023-06-02 17:28:53 +0300

    Initial commit

---
+++ ASP.NET-Fundamentals/Forum.App/Forum.App/Data/ForumDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Forum.App.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ASP.NET-Fundamentals/Forum.App/Forum.App/Data/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿namespace Forum.App.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Forum.App.Common.EntityValidations.Post;
+
+    public class Post
+    {
+        public Post()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; } = null!;
+    }
+}
