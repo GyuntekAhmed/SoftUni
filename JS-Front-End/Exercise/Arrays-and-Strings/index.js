9eb7b5829a2b32d60f81ff064d71be16452876cc
gyuntekahmed <gyuntekahmed@abv.bg>
2023-10-30 20:10:36 +0200

    Initial commit

---
+++ JS-Front-End/Exercise/Arrays-and-Strings/index.js
@@ -0,0 +1,29 @@
+// function rotation(arr, number) {
+
+//   for (let i = 0; i < number; i++) {
+//     const firstElement = arr.shift();
+//     arr.push(firstElement);
+//   }
+
+//   console.log(arr.join(' '));
+// }
+
+// rotation([51, 47, 32, 61, 21], 2);
+// rotation([32, 21, 61, 1], 4);
+// rotation([2, 4, 15, 31], 5);
+
+// function solve(arr, step) {
+//     let result = [];
+
+//     for (let i = 0; i < arr.length; i += step) {
+//         result.push(arr[i]);
+//     }
+
+//     console.log(result);
+// }
+
+// solve(['dsa',
+// 'asd', 
+// 'test', 
+// 'tset'], 
+// 2);
