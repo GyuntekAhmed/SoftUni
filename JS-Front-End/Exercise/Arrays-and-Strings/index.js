27a6165bf1329f00c8160546fb1e4643bca51043
gyuntekahmed <gyuntekahmed@abv.bg>
2023-10-31 18:47:55 +0200

    Initial commit

--- JS-Front-End/Exercise/Arrays-and-Strings/index.js
+++ JS-Front-End/Exercise/Arrays-and-Strings/index.js
@@ -27,3 +27,13 @@
 // 'test', 
 // 'tset'], 
 // 2);
+
+function printSortedNames(names) {
+    names.sort();
+
+    for (let i = 0; i < names.length; i++) {
+        console.log(`${i + 1}. ${names[i]}`);
+    }
+}
+
+printSortedNames(["John", "Bob", "Christina", "Ema"]);
