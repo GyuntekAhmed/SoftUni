02f515755d68a4935926b8ef83314d9007f2099d
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-02 20:08:49 +0200

    Initial commit

--- JS-Front-End/Exercise/Arrays-and-Strings/index.js
+++ JS-Front-End/Exercise/Arrays-and-Strings/index.js
@@ -28,12 +28,33 @@
 // 'tset'], 
 // 2);
 
-function printSortedNames(names) {
-    names.sort();
+// function printSortedNames(names) {
+//     names.sort();
 
-    for (let i = 0; i < names.length; i++) {
-        console.log(`${i + 1}. ${names[i]}`);
-    }
-}
+//     for (let i = 0; i < names.length; i++) {
+//         console.log(`${i + 1}. ${names[i]}`);
+//     }
+// }
 
-printSortedNames(["John", "Bob", "Christina", "Ema"]);
+// printSortedNames(["John", "Bob", "Christina", "Ema"]);
+
+// function sortArrayAlternating(numbers) {
+//     const sortedArray = numbers.sort((a, b) => a - b);
+//     const result = [];
+
+//     for (let i = 0, j = sortedArray.length - 1; i <= j; i++, j--) {
+//         if (i === j) {
+//             result.push(sortedArray[i]);
+//         } else {
+//             result.push(sortedArray[i], sortedArray[j]);
+//         }
+//     }
+
+//     return result;
+// }
+
+// // Example usage:
+// const numbersArray = [5, 2, 9, 1, 5, 6];
+// const resultArray = sortArrayAlternating(numbersArray);
+
+// console.log(resultArray); // This will print the sorted array with alternating elements
