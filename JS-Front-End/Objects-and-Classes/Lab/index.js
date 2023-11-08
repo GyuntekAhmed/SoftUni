e432a1e5e1fba7a91fe5cce601850b9ca47b9592
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-08 22:33:02 +0200

    Create folder ObjectsAndClasses and initial commit

---
+++ JS-Front-End/Objects-and-Classes/Lab/index.js
@@ -0,0 +1,27 @@
+// function personInfo(firstName, lastName, age) {
+//   const person = {
+//     firstName,
+//     lastName,
+//     age,
+//   }
+
+//   return person;
+// }
+
+// console.log("Peter", "Pan", "20");
+
+// function printCityInfo(city) {
+//   Object.entries(city).forEach(function ([key, value]) {
+//     console.log(key + " -> " + value);
+//   });
+// }
+
+// printCityInfo({
+//   name: "Plovdiv",
+//   area: 389,
+//   population: 1162358,
+//   country: "Bulgaria",
+//   postCode: "4000",
+// });
+
+
