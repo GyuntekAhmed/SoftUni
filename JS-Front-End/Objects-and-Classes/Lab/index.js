4da9eb1a296131decf20bdf36fa855b155cdb1cc
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-09 22:36:00 +0200

    Initial commit

--- JS-Front-End/Objects-and-Classes/Lab/index.js
+++ JS-Front-End/Objects-and-Classes/Lab/index.js
@@ -24,4 +24,50 @@
 //   postCode: "4000",
 // });
 
+// function printJSONKeysAndValues(jsonString) {
+//   const jsonObject = JSON.parse(jsonString);
 
+//   for (const key in jsonObject) {
+//     const value = jsonObject[key];
+//     console.log(`${key}: ${value}`);
+//   }
+// }
+
+// const jsonStr = '{"name": "George", "age": 40, "town": "Sofia"}';
+// printJSONKeysAndValues(jsonStr);
+
+// function convertToJson(name, lastName, hairColor) {
+//     const person = {
+//         name,
+//         lastName,
+//         hairColor,
+//     };
+
+//     console.log(JSON.stringify(person));
+// }
+
+// convertToJson('George', 'Jones', 'Brown');
+
+function createCity(name, population, treasury) {
+  return {
+    name,
+    population,
+    treasury,
+    taxRate: 10,
+    colletcTaxes() {
+        this.treasury += this.population * this.taxRate;
+    },
+    applyGrowth(percentage) {
+        this.population += this.population * percentage;
+    },
+    applyRecession(percentage) {
+        this.treasury -= this.treasury * percentage;
+    },
+  };
+}
+
+const city = createCity('Tortuga', 7000, 15000);
+city.colletcTaxes();
+console.log(city.treasury);
+city.applyGrowth(0.05);
+console.log(city.population);
