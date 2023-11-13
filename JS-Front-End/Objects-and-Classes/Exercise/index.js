1ed0d7441494ffb00435049c1ce8593ea1a26bc0
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-13 18:18:22 +0200

    Initial commit

--- JS-Front-End/Objects-and-Classes/Exercise/index.js
+++ JS-Front-End/Objects-and-Classes/Exercise/index.js
@@ -251,4 +251,34 @@
 // console.log(vehicle.fuel);
 // console.log(vehicle.parts.quality);
 
+function solve(input) {
+  const products = input.reduce((acc, curr) => {
+    const [key, value] = curr.split(" : ");
+    acc[key] = Number(value);
 
+    return acc;
+  }, {});
+
+  const sortedKeys = Object.keys(products).sort();
+
+  let letter = sortedKeys[0][0];
+  console.log(letter);
+  sortedKeys.forEach((key) => {
+    if (key[0] !== letter) {
+      letter = key[0];
+      console.log(letter);
+    }
+    console.log(`  ${key}: ${products[key]}`);
+  });
+}
+
+solve([
+  "Appricot : 20.4",
+  "Fridge : 1500",
+  "TV : 1499",
+  "Deodorant : 10",
+  "Boiler : 300",
+  "Apple : 1.25",
+  "Anti-Bug Spray : 15",
+  "T-Shirt : 10",
+]);
