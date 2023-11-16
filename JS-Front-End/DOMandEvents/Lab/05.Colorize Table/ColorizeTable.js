17dc698c4a749a688aed2c1c5bfa579efcad890d
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-16 19:43:21 +0200

    Initial commit

--- JS-Front-End/DOMandEvents/Lab/05.Colorize Table/ColorizeTable.js
+++ JS-Front-End/DOMandEvents/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,7 @@
 function colorize() {
-    // TODO
+    const rows = Array.from(document.querySelectorAll("tr:nth-child(even)"));
+
+    rows.forEach(row => {
+        row.style.background = "teal";
+    })
 }
--- JS-Front-End/DOMandEvents/Lab/06.Sum Table/SumTable.js
+++ JS-Front-End/DOMandEvents/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
+    const prices = Array.from(document.querySelectorAll("td:nth-child(2)"));
 
+    const total = prices.reduce((acc, current) => {
+        return acc + Number(current.textContent);
+    }, 0);
+
+    document.querySelector("#sum").textContent = total;
 }
