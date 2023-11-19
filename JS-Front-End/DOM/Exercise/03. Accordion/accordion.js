3034ebef495cb4a992309f3762cc8e63197ff720
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-19 12:33:02 +0200

    Initial commit

--- JS-Front-End/DOM/Exercise/03. Accordion/accordion.js
+++ JS-Front-End/DOM/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,7 @@
 function toggle() {
-    console.log('TODO:...');
+  const content = document.querySelector("#extra");
+  const button = document.querySelector("span.button");
+
+  content.style.display = content.style.display === "block" ? "none" : "block";
+  button.textContent = button.textContent === "More" ? "Less" : "More";
 }
