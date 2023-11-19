a183c8b92cb19b34ba6aad45e9e5017bc7931684
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-19 15:15:49 +0200

    Initial commit

--- JS-Front-End/DOM/Exercise/06. Format the Text/solution.js
+++ JS-Front-End/DOM/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+  const input = document.querySelector("#input").value.split(".");
+  input.pop();
+  const container = document.querySelector("#output");
+
+  while (input.length > 0) {
+    const p = document.createElement("p");
+    p.textContent = input
+      .splice(0, 3)
+      // .map((text) => text.trim())
+      .join(".");
+    container.appendChild(p);
+  }
+
 }
--- JS-Front-End/DOMManipulationAndEvents/Exercise/06. Furniture/solution.js
+++ JS-Front-End/DOMManipulationAndEvents/Exercise/06. Furniture/solution.js
@@ -1,4 +1,48 @@
 function solve() {
+  const button = document.querySelector("#exercise button");
+  button.addEventListener("click", parseFurnitureInput);
 
-  //TODO...
+  function parseFurnitureInput() {
+    const input = JSON.parse(
+      document.querySelector("#exercise textarea").value
+    );
+
+    const tableBody = document.querySelector("tbody");
+
+    input
+      .map((furniture) => {
+        const row = document.createElement("tr");
+        const imageCell = document.createElement("td");
+        const image = document.createElement("img");
+
+        image.src = furniture.img;
+        imageCell.appendChild(image);
+
+        const nameCell = document.createElement("td");
+        nameCell.textContent = furniture.name;
+
+        const priceCell = document.createElement("td");
+        priceCell.textContent = furniture.price;
+
+        const decorationFactorCell = document.createElement("td");
+        decorationFactorCell.textContent = furniture.decFactor;
+
+        const chekCell = document.createElement("td");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        chekCell.appendChild(checkbox);
+
+        row.appendChild(imageCell);
+        row.appendChild(nameCell);
+        row.appendChild(priceCell);
+        row.appendChild(decorationFactorCell);
+        row.appendChild(chekCell);
+
+
+        return row;
+      })
+      .forEach((row) => {
+        tableBody.appendChild(row);
+      });
+  }
 }
