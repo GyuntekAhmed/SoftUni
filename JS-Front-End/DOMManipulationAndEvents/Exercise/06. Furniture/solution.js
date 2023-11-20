dc8a311ed2cb2e83faa001a4ca4386a96815499b
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-20 18:56:25 +0200

    Initial commit

--- JS-Front-End/DOMManipulationAndEvents/Exercise/06. Furniture/solution.js
+++ JS-Front-End/DOMManipulationAndEvents/Exercise/06. Furniture/solution.js
@@ -1,6 +1,13 @@
 function solve() {
-  const button = document.querySelector("#exercise button");
-  button.addEventListener("click", parseFurnitureInput);
+  const formatButton = document.querySelector("#exercise button");
+  formatButton.addEventListener("click", parseFurnitureInput);
+
+  const buyButton = document.querySelector("#exercise button:nth-of-type(2)");
+  buyButton.addEventListener("click", buySelectedFurniture);
+
+  Array.from(document.querySelectorAll('input[type="checkbox"]')).forEach(
+    (checkbox) => checkbox.removeAttribute("disabled")
+  );
 
   function parseFurnitureInput() {
     const input = JSON.parse(
@@ -38,11 +45,50 @@ function solve() {
         row.appendChild(decorationFactorCell);
         row.appendChild(chekCell);
 
-
         return row;
       })
       .forEach((row) => {
         tableBody.appendChild(row);
       });
+  }
+
+  function buySelectedFurniture() {
+    const checkboxes = Array.from(
+      document.querySelectorAll('input[type="checkbox"]:checked')
+    );
+
+    const cart = checkboxes.map(mapCheckboxToObject).reduce(
+      (acc, current) => {
+        acc.names.push(current.name);
+        acc.price += current.price;
+        acc.averageDecorationFactor += current.decFactor / checkboxes.length;
+
+        return acc;
+      },
+      {
+        names: [],
+        price: 0,
+        averageDecorationFactor: 0,
+      }
+    );
+
+    const cartTextArea = document.querySelector(
+      "#exercise textarea:nth-of-type(2)"
+    );
+
+    cartTextArea.value = `
+      Bought furniture: ${cart.names.join(", ")}
+      Total price: ${cart.price.toFixed(2)}
+      Average decoration factor: ${cart.averageDecorationFactor.toFixed(2)}
+    `;
+  }
+
+  function mapCheckboxToObject(checkbox) {
+    const row = checkbox.parentElement.parentElement;
+    const name = row.querySelector("td:nth-of-type(2)").innerText;
+    const price = Number(row.querySelector("td:nth-of-type(3)").innerText);
+    const decFactor = Number(row.querySelector("td:nth-of-type(4)").innerText);
+
+    return { name, price, decFactor };
   }
 }
