8f978a96c64427d13969ba4e3b5fb5b07244d0a4
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-23 18:31:00 +0200

    Initial commit

--- JS-Front-End/DOM/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS-Front-End/DOM/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    const text = document.getElementById(content).textContent;
 
+    const reg = /\((.+?)\)/g;
+    let match = reg.exec(text);
+    let result = [];
+
+    while (match !== null) {
+        result.push(match[1]);
+        match = reg.exec(text);
+    }
+
+    return result.join("; ")
 }
