f2c918a2399e62a7c52a5267e9361eaec6d179f2
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-24 18:53:58 +0200

    Initial commit

--- JS-Front-End/DOM/Exercise/02. Pascal or Camel Case/solution.js
+++ JS-Front-End/DOM/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+  let input = document.querySelector("#text").value.toLowerCase();
+  let command = document.querySelector("#naming-convention").value;
+  let result = document.querySelector("#result");
+
+  if (command === "Camel Case") {
+    let sentence = [];
+
+    input = input.split(" ");
+    sentence.push(input[0]);
+
+    for (let i = 1; i < input.length; i++) {
+      sentence.push(input[i].charAt(0).toUpperCase() + input[i].slice(1));
+    }
+
+    result.textContent = sentence.join("");
+  } else if (command === "Pascal Case") {
+    let sentence = [];
+    input = input.split(" ");
+    for (let i = 0; i < input.length; i++) {
+      sentence.push(input[i].charAt(0).toUpperCase() + input[i].slice(1));
+    }
+
+    result.textContent = sentence.join("");
+  } else {
+    result.textContent = "Error!"
+  }
 }
