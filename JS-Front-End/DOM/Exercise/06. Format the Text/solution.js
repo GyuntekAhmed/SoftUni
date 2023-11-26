25b05859def8988b9f4543c837268ddc3e931f9b
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-26 12:04:07 +0200

    Initial commit

--- JS-Front-End/DOM/Exercise/06. Format the Text/solution.js
+++ JS-Front-End/DOM/Exercise/06. Format the Text/solution.js
@@ -1,15 +1,13 @@
 function solve() {
-  const input = document.querySelector("#input").value.split(".");
-  input.pop();
-  const container = document.querySelector("#output");
+  let input = document.getElementById('input').value;
+  let container = document.getElementById('output');
+  let sentences = input.split('.');
 
-  while (input.length > 0) {
-    const p = document.createElement("p");
-    p.textContent = input
-      .splice(0, 3)
-      // .map((text) => text.trim())
-      .join(".");
+  sentences = sentences.filter(s => s.length > 0).map(s => s += '.');
+
+  while (sentences.length > 0) {
+    let p = document.createElement("p");
+    p.textContent = sentences.splice(0, 3).join("");
     container.appendChild(p);
   }
-
 }
