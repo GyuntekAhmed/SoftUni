d3d24f51aef25627b8b359fa00434c32356a83a0
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-26 16:45:31 +0200

    Initial commit

--- JS-Front-End/HTTP-and-REST/06. Github-Commits/app.js
+++ JS-Front-End/HTTP-and-REST/06. Github-Commits/app.js
@@ -2,7 +2,21 @@ function loadCommits() {
   const username = document.querySelector("#username").value;
   const repo = document.querySelector("#repo").value;
 
+  if (!username || !repo) {
+    return;
+  }
+
+  const list = document.querySelector("ul");
+  list.innerHTML = "";
+
   fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
     .then((res) => res.json())
-    .then(console.log);
+    .then((commits) => {
+      commits.forEach(({ commit }) => {
+        const item = document.createElement("li");
+        item.textContent = commit.message;
+
+        list.appendChild(item);
+      });
+    });
 }
