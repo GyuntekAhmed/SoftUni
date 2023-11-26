5957e92697b06a2d2a03e1a331f019111d57069a
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-26 12:51:54 +0200

    Initial commit

--- JS-Front-End/DOM/Exercise/08. Generate Report/report.js
+++ JS-Front-End/DOM/Exercise/08. Generate Report/report.js
@@ -1,3 +1,41 @@
 function generateReport() {
-    //TODO
+    let personObject = {};
+    let personAllObjects = [];
+    let columnSetNumbers = [];
+
+    let columsAll = document.querySelectorAll('thead tr th input');
+
+    for (let i = 0; i < columsAll.length; i++) {
+        let checkboxState = columsAll[i].checked;
+        
+        if (checkboxState) {
+            columnSetNumbers.push(i);
+        }
+    }
+
+    let columnTitles = document
+        .querySelectorAll('thead tr')[0]
+            .getElementsByTagName('th');
+
+    let rowsCount = document.querySelectorAll('tbody tr').length;
+
+    for (let row = 0; row < rowsCount; row++) {
+        
+        columnSetNumbers.forEach((element) => {
+            let key = columnTitles[element]
+                .textContent.trim()
+                    .toLowerCase();
+            let value = document
+                .querySelectorAll('tbody tr')[row]
+                    .getElementsByTagName('td')[element]
+                        .textContent;
+            personObject[key] = value;
+        });
+
+        personAllObjects.push(Object.assign(personObject));
+        personObject = {};
+    }
+
+    document.getElementById('output')
+    .innerHTML = JSON.stringify(personAllObjects);
 }
