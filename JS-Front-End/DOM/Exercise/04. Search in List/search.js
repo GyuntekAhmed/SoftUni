086df6a039054240fd40e4372ef83f52f640a95b
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-26 11:41:36 +0200

    Initial commit

--- JS-Front-End/DOM/Exercise/04. Search in List/search.js
+++ JS-Front-End/DOM/Exercise/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+   let listItems = Array.from(document.querySelectorAll('li'));
+   let searchingText = document.getElementById('searchText').value;
+   let result = document.getElementById('result');
+   let matches = 0;
+
+   for (const li of listItems) {
+      li.style.fontWeight = '';
+      li.style.textDecoration = '';
+   }
+
+   for (const li of listItems) {
+      if (li.textContent.includes(searchingText)) {
+         li.style.fontWeight = 'bold';
+         li.style.textDecoration = 'underline';
+         matches++;
+      }
+   }
+
+   result.textContent = `${matches} matches found`;
 }
