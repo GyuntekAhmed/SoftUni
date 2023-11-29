b7a5f2319324045c5838b6f3713e32f3b4422be3
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-29 22:39:48 +0200

    Initial commit

--- JS-Front-End/DOMManipulationAndEvents/Lab/01. List-Of-Items/app.js
+++ JS-Front-End/DOMManipulationAndEvents/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+  const items = document.querySelector("#items");
+  const text = document.querySelector("#newItemText");
+  const newListItem = document.createElement("li");
+
+  newListItem.textContent = text.value;
+  items.appendChild(newListItem);
+
+  text.value = '';
 }
