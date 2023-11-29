14d9a1523b4db9bd6bcf1e4b37070780ca052e36
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-29 17:56:04 +0200

    Modify app.js in Bus Schedule project

--- JS-Front-End/HTTP-and-REST/Exercise/03.Forecaster/app.js
+++ JS-Front-End/HTTP-and-REST/Exercise/03.Forecaster/app.js
@@ -1,5 +1,73 @@
 function attachEvents() {
-    // TODO:
+  document
+    .querySelector("#submit")
+    .addEventListener("click", getWeatherDataForLocation);
+}
+
+const WEATHERSYMBOLS = {
+  Sunny: "☀",
+  "Partly sunny": "⛅",
+  Overcast: "☁",
+  Rain: "☂",
+};
+
+async function getWeatherDataForLocation() {
+  const locationName = document.querySelector("#location").value;
+  const locationResponse = await (
+    await fetch("http://localhost:3030/jsonstore/forecaster/locations")
+  ).json();
+
+  const location = locationResponse.find(
+    (l) => l.name.toLowerCase() === locationName.toLowerCase()
+  );
+
+  const currentConditionsResponse = await (
+    await fetch(
+      `http://localhost:3030/jsonstore/forecaster/today/${location.code}`
+    )
+  ).json();
+
+  const threeDayForecastResponse = await (
+    await fetch(
+      `http://localhost:3030/jsonstore/forecaster/upcoming/${location.code}`
+    )
+  ).json();
+
+  const forecastContainer = document.querySelector("#forecast");
+  forecastContainer.style.display = "block";
+
+  const currentWeatherContainer = document.createElement("div");
+  currentWeatherContainer.classList.add("forecasts");
+
+  const weatherSymbol = document.createElement("span");
+  weatherSymbol.classList.add("condition", "symbol");
+
+  weatherSymbol.textContent =
+    WEATHERSYMBOLS[currentConditionsResponse.forecast.condition];
+
+  const dataHolder = document.createElement("span");
+  dataHolder.classList.add("condition");
+
+  const name = document.createElement("span");
+  name.textContent = currentConditionsResponse.name;
+  name.classList.add("forecast-data");
+  dataHolder.appendChild(name);
+
+  const temp = document.createElement("span");
+  temp.textContent = `${currentConditionsResponse.forecast.low}°/${currentConditionsResponse.forecast.high}°`;
+  temp.classList.add("forecast-data");
+  dataHolder.appendChild(temp);
+
+  const condition = document.createElement("span");
+  condition.textContent =
+    WEATHERSYMBOLS[currentConditionsResponse.forecast.condition];
+  condition.classList.add("forecast-data");
+  dataHolder.appendChild(condition);
+
+  currentWeatherContainer.appendChild(weatherSymbol);
+  currentWeatherContainer.appendChild(dataHolder);
+
+  document.querySelector("#current").appendChild(currentWeatherContainer);
 }
 
 attachEvents();
