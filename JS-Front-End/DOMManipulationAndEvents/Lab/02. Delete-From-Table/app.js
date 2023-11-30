a400e1360bf7bf5aa02c1287ebf70dfc4a76efc2
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-30 21:10:41 +0200

    Initial commit

--- JS-Front-End/DOMManipulationAndEvents/Lab/02. Delete-From-Table/app.js
+++ JS-Front-End/DOMManipulationAndEvents/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const input = document.querySelector('input[name="email"]');
+  const rows = Array.from(document.querySelectorAll("tbody tr"));
+  const result = document.querySelector("#result");
+  let isRemoved = false;
+
+  for (const row of rows) {
+    const email = row.children[1];
+
+    if (email.textContent === input.value) {
+      row.remove();
+      isRemoved = true;
+    }
+  }
+
+  if (isRemoved) {
+    result.textContent = "Deleted";
+  } else {
+    result.textContent = "Not found.";
+  }
 }
