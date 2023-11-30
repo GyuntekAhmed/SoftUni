6c8e1ae5acc82ed7cfdf74da6c5d3b8fee396e0d
gyuntekahmed <gyuntekahmed@abv.bg>
2023-11-30 21:29:16 +0200

    Initial commit

--- JS-Front-End/DOMManipulationAndEvents/Lab/03. Add-Delete/app.js
+++ JS-Front-End/DOMManipulationAndEvents/Lab/03. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
-}
+    const items = document.querySelector("#items");
+    const text = document.querySelector("#newItemText");
+    const newListItem = document.createElement("li");
+    const linkToDelete = document.createElement("a");
+
+    linkToDelete.href = "#";
+    linkToDelete.textContent = "[Delete]";
+  
+    linkToDelete.addEventListener('click', deleteItem);
+    newListItem.textContent = text.value;
+    newListItem.appendChild(linkToDelete);
+
+    items.appendChild(newListItem);
+  
+    text.value = '';
+
+
+    function deleteItem(e) {
+        const row = e.currentTarget.parentNode;
+        row.remove();
+    }
+  }
