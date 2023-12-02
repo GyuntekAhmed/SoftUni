442fcefdd9e2644fff5a15ac59ea2ec1515defdb
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-02 15:28:15 +0200

    Initial commit

--- JS-Front-End/DOMManipulationAndEvents/Exercise/07. Distance-Converter/distanceConverter.js
+++ JS-Front-End/DOMManipulationAndEvents/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,30 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+  let [inputDistanceField, convertBtn, outputDistanceField] =
+    document.getElementsByTagName("input");
+
+  let [inputUnitOption, outputUnitOption] =
+    document.getElementsByTagName("select");
+
+  let convertionRates = {
+    'km': 1000,
+    'm': 1,
+    'cm': 0.01,
+    'mm': 0.001,
+    'mi': 1609.34,
+    'yrd': 0.9144,
+    'ft': 0.3048,
+    'in': 0.0254,
+  };
+
+  convertBtn.addEventListener("click", () => {
+    let inputDistance = inputDistanceField.value;
+    let inputUnits = inputUnitOption.value;
+    let outputUnits = outputUnitOption.value;
+
+    let result =
+      (inputDistance * convertionRates[inputUnits]) /
+      convertionRates[outputUnits];
+
+    outputDistanceField.value = result;
+  });
 }
