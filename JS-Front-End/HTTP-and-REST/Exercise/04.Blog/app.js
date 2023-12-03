6070bb833015a906051641f967da2989730ea6c5
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-03 10:18:58 +0200

    Start blog project and initial commit

--- JS-Front-End/HTTP-and-REST/Exercise/04.Blog/app.js
+++ JS-Front-End/HTTP-and-REST/Exercise/04.Blog/app.js
@@ -1,5 +1,39 @@
+let posts;
+
 function attachEvents() {
-    // TODO:
+  document.querySelector("#btnLoadPosts").addEventListener("click", loadPosts);
+  document
+    .querySelector("#btnViewPost")
+    .addEventListener("click", loadSinglePost);
+}
+
+async function loadSinglePost() {
+  const result = await (
+    await fetch("http://localhost:3030/jsonstore/blog/comments")
+  ).json();
+
+  const selectedPost = posts[document.querySelector("#posts").value];
+  const comments = Object.values(result).filter(
+    (comment) => comment.postId === selectedPost.id
+  );
+}
+
+async function loadPosts() {
+  const result = await (
+    await fetch("http://localhost:3030/jsonstore/blog/posts")
+  ).json();
+
+  posts = result;
+
+  const postsDropdown = document.querySelector("#posts");
+
+  Object.values(result).forEach((post) => {
+    const option = document.createElement("option");
+    option.value = post.id;
+    option.text = post.title;
+
+    postsDropdown.appendChild(option);
+  });
 }
 
 attachEvents();
