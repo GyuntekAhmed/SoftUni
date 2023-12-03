0597dbb73120b823a7a336dfa7f38deab231e801
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-03 11:18:13 +0200

    Initial commit

--- JS-Front-End/HTTP-and-REST/Exercise/05.Messenger/app.js
+++ JS-Front-End/HTTP-and-REST/Exercise/05.Messenger/app.js
@@ -1,5 +1,30 @@
 function attachEvents() {
-    // TODO:
+  document.querySelector("#refresh").addEventListener("click", getMessages);
+  document.querySelector("#submit").addEventListener("click", sendMessage);
+}
+
+async function sendMessage() {
+  const author = document.querySelector('input[name="author"]').value;
+  const content = document.querySelector('input[name="content"]').value;
+
+  const response = await fetch("http://localhost:3030/jsonstore/messenger", {
+    method: "POST",
+    body: JSON.stringify({ author, content }),
+  });
+  return response;
+}
+
+async function getMessages() {
+  const messages = await (
+    await fetch("http://localhost:3030/jsonstore/messenger")
+  ).json();
+
+  const messageBox = document.querySelector("#messages");
+  messageBox.textContent = "";
+  
+  Object.values(messages).forEach((message) => {
+      messageBox.textContent += `${message.author}: ${message.content}\n`;
+    });
 }
 
 attachEvents();
