a7fc214fd1327c4d53487ee9f3e9ad79a5127ef2
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-04 17:51:03 +0200

    Initial commit

--- JS-Front-End/HTTP-and-REST/Exercise/06.Phonebook/app.js
+++ JS-Front-End/HTTP-and-REST/Exercise/06.Phonebook/app.js
@@ -1,5 +1,16 @@
 function attachEvents() {
-    // TODO:
+  document.querySelector("#form button").addEventListener("click", saveBook);
+}
+
+async function saveBook() {
+  const name = document.querySelector('#form input[name="title"]').value;
+  const author = document.querySelector('#form input[name="author"]').value;
+
+  if (!name || !author) {
+    return;
+  }
+
+  fetch()
 }
 
 attachEvents();
