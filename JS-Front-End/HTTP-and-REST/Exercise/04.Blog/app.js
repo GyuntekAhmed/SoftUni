e11e8e1c3cc09cac265528d0e7762e52caf17080
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-08 17:57:41 +0200

    Initial commit

--- JS-Front-End/HTTP-and-REST/Exercise/04.Blog/app.js
+++ JS-Front-End/HTTP-and-REST/Exercise/04.Blog/app.js
@@ -16,6 +16,17 @@ async function loadSinglePost() {
   const comments = Object.values(result).filter(
     (comment) => comment.postId === selectedPost.id
   );
+  document.getElementById("post-title").innerText = selectedPost.title;
+  document.getElementById("post-body").innerText = selectedPost.body;
+
+  const ul = document.getElementById("post-comments");
+  ul.innerHTML = "";
+
+  comments.forEach((comment) => {
+    const li = document.createElement("li");
+    li.textContent = comment.text;
+    ul.appendChild(li);
+  });
 }
 
 async function loadPosts() {
