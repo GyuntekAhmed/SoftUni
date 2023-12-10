11782040fee063d32623866af12a5084053185b1
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-10 11:03:18 +0200

    Add functionality to Phonebook solution

--- JS-Front-End/AJAX/03.Phonebook/app.js
+++ JS-Front-End/AJAX/03.Phonebook/app.js
@@ -1,5 +1,57 @@
 function attachEvents() {
-    // TODO:
+  const baseURL = "http://localhost:3030/jsonstore/phonebook";
+
+  document.getElementById("btnLoad").addEventListener("click", loadAllContacts);
+  document.getElementById("btnCreate").addEventListener("click", createContact);
+
+  const person = document.getElementById("person");
+  const phone = document.getElementById("phone");
+
+  async function loadAllContacts() {
+    const phonesInfo = await (await fetch(baseURL)).json();
+
+    const ul = document.getElementById("phonebook");
+
+    Object.values(phonesInfo).forEach((contact) => {
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+
+      const li = document.createElement("li");
+      li.textContent = `${contact.person}: ${contact.phone}`;
+      li.appendChild(deleteButton);
+
+      ul.appendChild(li);
+
+      const contactId = contact._id;
+
+      deleteButton.addEventListener("click", deleteContact);
+
+      function deleteContact() {
+        fetch(`${baseURL}/${contactId}`, {
+          method: "DELETE",
+        });
+
+        li.remove();
+        loadAllContacts();
+      }
+    });
+  }
+
+  async function createContact() {
+    const contactFormat = {
+      person: person.value,
+      phone: phone.value,
+    };
+
+    await fetch(baseURL, {
+      method: "POST",
+      body: JSON.stringify(contactFormat),
+    });
+
+    document.getElementById("person").value = "";
+    document.getElementById("phone").value = "";
+    loadAllContacts();
+  }
 }
 
 attachEvents();
