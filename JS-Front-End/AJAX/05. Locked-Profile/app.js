13aebc559bbbec77e75b3a40a451f5cc693e3375
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-11 22:56:53 +0200

    Add demo  solutions for exam

--- JS-Front-End/AJAX/05. Locked-Profile/app.js
+++ JS-Front-End/AJAX/05. Locked-Profile/app.js
@@ -1,15 +1,15 @@
 async function lockedProfile() {
-    const baseURL = 'http://localhost:3030/jsonstore/advanced/profiles';
+  const baseURL = "http://localhost:3030/jsonstore/advanced/profiles";
 
-    const response = await fetch(baseURL);
+  const response = await fetch(baseURL);
 
-    const data = await response.json();
+  const data = await response.json();
 
-    Object.values(data).forEach(profile => {
-        const userName = profile.username.value;
+  Object.values(data).forEach((profile) => {
+    const userName = profile.username.value;
 
-        let info = document.querySelector(`input[name="user1Username"]`);
+    let info = document.querySelector(`input[name="user1Username"]`);
 
-        info.textContent = userName;
-    })
+    info.textContent = userName;
+  });
 }
