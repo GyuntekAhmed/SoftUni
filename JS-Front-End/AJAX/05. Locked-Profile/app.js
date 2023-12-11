f5d94d66fd14923c9a40a06515404ec602d82acb
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-11 18:06:32 +0200

    Initial commit

--- JS-Front-End/AJAX/05. Locked-Profile/app.js
+++ JS-Front-End/AJAX/05. Locked-Profile/app.js
@@ -1,3 +1,15 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    const baseURL = 'http://localhost:3030/jsonstore/advanced/profiles';
+
+    const response = await fetch(baseURL);
+
+    const data = await response.json();
+
+    Object.values(data).forEach(profile => {
+        const userName = profile.username.value;
+
+        let info = document.querySelector(`input[name="user1Username"]`);
+
+        info.textContent = userName;
+    })
 }
