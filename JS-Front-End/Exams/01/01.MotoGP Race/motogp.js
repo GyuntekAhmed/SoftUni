c2a8314696ab891feaf7e3feff1562418d597fec
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-12 00:05:56 +0200

    Initial commit

--- JS-Front-End/Exams/01/01.MotoGP Race/motogp.js
+++ JS-Front-End/Exams/01/01.MotoGP Race/motogp.js
@@ -1,1 +1,19 @@
-//TODO...
+function raceActions(input) {
+    const lines = Number(input.shift());
+
+    const riders = input.slice(0, lines);
+    const actions = input.slice(lines);
+    
+  }
+  
+  
+  
+  raceActions(["3",
+  "Valentino Rossi|100|1",
+  "Marc Marquez|90|2",
+  "Jorge Lorenzo|80|3",
+  "StopForFuel - Valentino Rossi - 50 - 1",
+  "Overtaking - Marc Marquez - Jorge Lorenzo",
+  "EngineFail - Marc Marquez - 10",
+  "Finish"]);
+  
