f2b2fb4b4f1e0c9f20ca00ab28de4b366dc00dd5
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-15 00:52:00 +0200

    Add solutions for exam prep

--- JS-Front-End/Exams/02/01.Astro Adventure/astroAdventure.js
+++ JS-Front-End/Exams/02/01.Astro Adventure/astroAdventure.js
@@ -1,1 +1,103 @@
-//TODO...
+function solve(input) {
+  const countOfLines = Number(input.shift());
+  const details = Array.from(input.slice(0, countOfLines));
+  const commands = input.slice(countOfLines);
+
+  const astronauts = details.reduce((acc, curr) => {
+    const [astronaut, oxygen, energy] = curr.split(" ");
+
+    acc[astronaut] = {
+      oxygen: Number(oxygen),
+      energy: Number(energy),
+    };
+    return acc;
+  }, {});
+
+  const commandExecutor = {
+    Explore: explore,
+    Refuel: refuel,
+    Breathe: breathe,
+    End: end,
+  };
+
+  commands.forEach((command) => {
+    const [commandName, ...rest] = command.split(" - ");
+
+    commandExecutor[commandName](...rest);
+  });
+
+  function explore(astronaut, neededEnergy) {
+    if (astronauts[astronaut].energy >= Number(neededEnergy)) {
+      astronauts[astronaut].energy -= Number(neededEnergy);
+
+      console.log(
+        `${astronaut} has successfully explored a new area and now has ${astronauts[astronaut].energy} energy!`
+      );
+    } else {
+      console.log(`${astronaut} does not have enough energy to explore!`);
+    }
+  }
+
+  function refuel(astronaut, amount) {
+    let result = astronauts[astronaut].energy + Number(amount);
+
+    if (result <= 200) {
+      astronauts[astronaut].energy += Number(amount);
+      console.log(`${astronaut} refueled their energy by ${amount}!`);
+    } else {
+      astronauts[astronaut].energy = 200;
+      result = result - 200;
+      console.log(`${astronaut} refueled their energy by ${amount - result}!`);
+    }
+  }
+
+  function breathe(astronaut, amount) {
+    let result = astronauts[astronaut].oxygen + Number(amount);
+
+    if (result <= 100) {
+      astronauts[astronaut].oxygen += Number(amount);
+      console.log(`${astronaut} took a breath and recovered ${amount} oxygen!`);
+    } else {
+      astronauts[astronaut].oxygen = 100;
+      result = result - 100;
+      console.log(
+        `${astronaut} took a breath and recovered ${amount - result} oxygen!`
+      );
+    }
+  }
+
+  function end() {
+    Object.keys(astronauts).forEach((astronaut) => {
+      console.log(
+        `Astronaut: ${astronaut}, Oxygen: ${astronauts[astronaut].oxygen}, Energy: ${astronauts[astronaut].energy}`
+      );
+    });
+  }
+}
+
+solve([
+  "3",
+  "John 50 120",
+  "Kate 80 180",
+  "Rob 70 150",
+  "Explore - John - 50",
+  "Refuel - Kate - 30",
+  "Breathe - Rob - 20",
+  "End",
+]);
+
+solve([
+  "4",
+  "Alice 60 100",
+  "Bob 40 80",
+  "Charlie 70 150",
+  "Dave 80 180",
+  "Explore - Bob - 60",
+  "Refuel - Alice - 30",
+  "Breathe - Charlie - 50",
+  "Refuel - Dave - 40",
+  "Explore - Bob - 40",
+  "Breathe - Charlie - 30",
+  "Explore - Alice - 40",
+  "End",
+]);
--- JS-Front-End/Exams/02/02. Dart Scoring/app.js
+++ JS-Front-End/Exams/02/02. Dart Scoring/app.js
@@ -1,6 +1,112 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO
+  const addButton = document.getElementById("add-btn");
+
+  const sureList = document.getElementById("sure-list");
+
+  const playerNameInput = document.getElementById("player");
+  const scoreInput = document.getElementById("score");
+  const roundInput = document.getElementById("round");
+
+  addButton.addEventListener("click", addInputsFromForm);
+
+  function addInputsFromForm(e) {
+    e.preventDefault();
+
+    if (
+      playerNameInput.value === "" ||
+      scoreInput.value === "" ||
+      roundInput.value === ""
+    ) {
+      return;
+    }
+
+    const li = createElement("li", null, "dart-item", null, sureList);
+    const article = createElement("article");
+    const playerParagraph = createElement("p", playerNameInput.value);
+    const scoreParagraph = createElement("p", `Score: ${scoreInput.value}`);
+    const roundParagraph = createElement("p", `Round: ${roundInput.value}`);
+
+    article.appendChild(playerParagraph);
+    article.appendChild(scoreParagraph);
+    article.appendChild(roundParagraph);
+    li.appendChild(article);
+
+    const editButton = createElement("button", "edit", "btn");
+    editButton.classList.add("edit");
+    li.appendChild(editButton);
+
+    addButton.disabled = true;
+
+    const currentPlayerValue = playerNameInput.value;
+    const currentScoreValue = scoreInput.value;
+    const currentRoundValue = roundInput.value;
+
+    document.getElementById("player").value = "";
+    document.getElementById("score").value = "";
+    document.getElementById("round").value = "";
+
+    editButton.addEventListener("click", () => {
+      playerNameInput.value = currentPlayerValue;
+      scoreInput.value = currentScoreValue;
+      roundInput.value = currentRoundValue;
+
+      sureList.innerHTML = "";
+      addButton.disabled = false;
+    });
+
+    const okButton = createElement("button", "ok", "btn");
+    okButton.classList.add("ok");
+    li.appendChild(okButton);
+
+    const scoreBoardList = document.getElementById("scoreboard-list");
+
+    okButton.addEventListener("click", () => {
+      scoreBoardList.appendChild(li);
+
+      li.removeChild(editButton);
+      li.removeChild(okButton);
+
+      sureList.innerHTML = "";
+      addButton.disabled = false;
+    });
   }
-  
+
+  const clearButton = document.getElementsByClassName('btn clear')[0];
+
+  clearButton.addEventListener('click', () => {
+    location.reload();
+  })
+
+  function createElement(
+    type,
+    textContent,
+    classes,
+    id,
+    parrent,
+    useInnerHTML
+  ) {
+    const element = document.createElement(type);
+
+    if (useInnerHTML && textContent) {
+      element.innerHTML = textContent;
+    } else if (textContent) {
+      element.textContent = textContent;
+    }
+
+    if (classes && classes.length > 0) {
+      element.classList.add(classes);
+    }
+
+    if (id) {
+      element.setAttribute("id", id);
+    }
+
+    if (parrent) {
+      parrent.appendChild(element);
+    }
+
+    return element;
+  }
+}
