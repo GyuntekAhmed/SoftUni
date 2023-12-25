15aa8e5779e6d58c8248b67f78edd5b08e38dafc
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-25 15:51:02 +0200

    Implement my abstract  custom Stack

--- Data-Structures/Linear-Data-Structures/Problem02.Stack/Stack.cs
+++ Data-Structures/Linear-Data-Structures/Problem02.Stack/Stack.cs
@@ -8,41 +8,83 @@
     {
         private class Node
         {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
             
+            public Node(T element)
+            {
+                this.Element = element;
+            }
         }
 
         private Node top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; private set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item, this.top);
+
+            this.top = newNode;
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldTop = this.top;
+            this.top = oldTop.Next;
+
+            this.Count--;
+            return oldTop.Element;
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.top.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            foreach (var element in this)
+            {
+                if (item.Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.top;
+
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+            => this.GetEnumerator();
     }
 }
