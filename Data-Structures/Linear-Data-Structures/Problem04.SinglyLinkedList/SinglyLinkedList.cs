3852c7e017389ec4beae7224ae9df71cd522144d
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-25 17:14:21 +0200

    Implement functionality for mu abstract custom SinglyLinkedList

--- Data-Structures/Linear-Data-Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ Data-Structures/Linear-Data-Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -6,46 +6,146 @@
 
     public class SinglyLinkedList<T> : IAbstractLinkedList<T>
     {
-        public int Count => throw new NotImplementedException();
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+        }
+
+        private Node head;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            Node toInsert = new Node(item);
+            toInsert.Next = this.head;
+            this.head = toInsert;
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
-        }
+            // 01. Create node
+            // 02. Iterate n elements and add to the .Next for the last
+            // 03. Increase Count
+            Node toInsert = new Node(item);
+            Node current = this.head;
+            if (current == null)
+            {
+                this.head = toInsert;
+            }
+            else
+            {
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
+                current.Next = toInsert;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNonEmptyList();
+
+            return this.head.Element;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNonEmptyList();
+
+            Node current = this.head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current.Element;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNonEmptyList();
+            Node current = this.head;
+            this.head = this.head.Next;
+            this.Count--;
+
+            return current.Element;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNonEmptyList();
+            Node current = this.head;
+            Node last = null;
+            // Works only for 1 element in the list
+            if (current.Next == null)
+            {
+                last = this.head;
+                this.head = null;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    // Works if there are more than 1 elements in the list
+                    if (current.Next.Next == null)
+                    {
+                        last = current.Next;
+                        current.Next = null;
+                        break;
+                    }
+
+                    current = current.Next;
+                }
+            }
+
+            this.Count--;
+            return last.Element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = this.head;
+
+            while (current != null)
+            {
+                yield return current.Element;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void EnsureNonEmptyList()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Linked List is empty!");
+            }
         }
     }
 }
