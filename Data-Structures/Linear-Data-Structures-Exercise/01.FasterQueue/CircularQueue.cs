fe44b0e15468e470a7d9d18142b844c9ec61eeaa
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-26 18:45:16 +0200

    Implement custom Circular Queue

--- Data-Structures/Linear-Data-Structures-Exercise/01.FasterQueue/CircularQueue.cs
+++ Data-Structures/Linear-Data-Structures-Exercise/01.FasterQueue/CircularQueue.cs
@@ -6,36 +6,86 @@
 
     public class CircularQueue<T> : IAbstractQueue<T>
     {
-        public int Count => throw new NotImplementedException();
+        private T[] elements;
+        private int startIndex;
+        private int endIndex;
+
+        public int Count { get; private set; }
+        public int Capacity { get => elements.Length; }
+
+        private const int InitialCapacity = 4;
+
+        public CircularQueue(int capacity = InitialCapacity)
+        {
+            this.elements = new T[capacity];
+        }
+
+        public void Enqueue(T element)
+        {
+            if (this.Count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+            this.elements[this.endIndex] = element;
+            this.endIndex = (this.endIndex + 1) % this.elements.Length;
+            this.Count++;
+        }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
-        }
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty!");
+            }
 
-        public void Enqueue(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        public T Peek()
-        {
-            throw new NotImplementedException();
+            var result = this.elements[this.startIndex];
+            this.startIndex = (this.startIndex + 1) % this.elements.Length;
+            this.Count--;
+            return result;
         }
 
         public T[] ToArray()
         {
-            throw new NotImplementedException();
+            return this.CopyAllElementsTo(new T[this.Count]);
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        public T Peek()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.elements[this.startIndex];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.elements[(this.startIndex + i) % this.elements.Length];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void Grow()
+        {
+            this.elements = this.CopyAllElementsTo(new T[2 * this.elements.Length]);
+            this.startIndex = 0;
+            this.endIndex = this.Count;
+        }
+
+        private T[] CopyAllElementsTo(T[] resultArr)
+        {
+            int sourceIndex = this.startIndex;
+            for (int i = 0; i < this.Count; i++)
+            {
+                resultArr[i] = this.elements[sourceIndex];
+                sourceIndex = (sourceIndex + 1) % this.elements.Length;
+            }
+
+            return resultArr;
         }
     }
 
