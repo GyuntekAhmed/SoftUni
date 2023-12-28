5f3afa4d8c04bc65f666d8a7e06736c758140974
gyuntekahmed <gyuntekahmed@abv.bg>
2023-12-28 19:12:40 +0200

    Initial commit

--- Data-Structures/Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Data-Structures/Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,39 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(parentheses) || parentheses.Length % 2 == 1)
+                return false;
+
+            var stack = new Stack<char>(parentheses.Length / 2);
+
+            foreach (var currentCharacter in parentheses)
+            {
+                char expectedCharacter = default;
+
+                switch (currentCharacter)
+                {
+                    case ')':
+                        expectedCharacter = '(';
+                        break;
+                    case ']':
+                        expectedCharacter = '[';
+                        break;
+                    case '}':
+                        expectedCharacter = '{';
+                        break;
+                    default:
+                        stack.Push(currentCharacter);
+                        break;
+                }
+
+                if (expectedCharacter == default)
+                    continue;
+
+                if (stack.Pop() != expectedCharacter)
+                    return false;
+            }
+
+            return stack.Count == 0;
         }
     }
 }
