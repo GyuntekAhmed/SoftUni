53812093d3f7fd9208b19ab21806a98d4cdb271f
gyuntekahmed <gyuntekahmed@abv.bg>
2024-01-16 21:52:08 +0200

    Initial commit

--- Data-Structures/Heaps-BST-Exercise-Exercises/02.LowestCommonAncestor/BinaryTree.cs
+++ Data-Structures/Heaps-BST-Exercise-Exercises/02.LowestCommonAncestor/BinaryTree.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     public class BinaryTree<T> : IAbstractBinaryTree<T>
         where T : IComparable<T>
@@ -36,7 +35,78 @@
 
         public T FindLowestCommonAncestor(T first, T second)
         {
-            throw new NotImplementedException();
+            var firstNode = this.FindNodeBfs(first, this);
+            var secondNode = this.FindNodeBfs(second, this);
+
+            if (firstNode == null || secondNode == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var firstNodeAncestors = this.FindNodeAllAncestors(firstNode);
+            var secondNodeAncestors = this.FindNodeAllAncestors(secondNode);
+
+            var currentEl = firstNodeAncestors.Dequeue();
+
+            while (firstNodeAncestors.Count > 0)
+            {
+                if (secondNodeAncestors.Contains(currentEl))
+                {
+                    return currentEl;
+                }
+
+                currentEl = firstNodeAncestors.Dequeue();
+            }
+
+            return currentEl;
+        }
+
+        private Queue<T> FindNodeAllAncestors(IAbstractBinaryTree<T> node)
+        {
+            var nodeAncestors = new Queue<T>();
+            var current = node;
+
+            while (current != null)
+            {
+                nodeAncestors.Enqueue(current.Value);
+                current = current.Parent;
+            }
+
+            return nodeAncestors;
+        }
+
+        private IAbstractBinaryTree<T> FindNodeBfs(T element, IAbstractBinaryTree<T> tree)
+        {
+            var queue = new Queue<IAbstractBinaryTree<T>>();
+
+            queue.Enqueue(tree);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                if (this.AreEqual(element, current.Value))
+                {
+                    return current;
+                }
+
+                if (current.LeftChild != null)
+                {
+                    queue.Enqueue(current.LeftChild);
+                }
+
+                if (current.RightChild != null)
+                {
+                    queue.Enqueue(current.RightChild);
+                }
+            }
+
+            return null;
+        }
+
+        private bool AreEqual(T first, T second)
+        {
+            return first.CompareTo(second) == 0;
         }
     }
 }
