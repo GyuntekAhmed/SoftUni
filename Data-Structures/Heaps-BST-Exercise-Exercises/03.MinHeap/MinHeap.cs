6ee1972e6daa9b596fdc7a3ff94c836983b377af
gyuntekahmed <gyuntekahmed@abv.bg>
2024-01-18 22:04:14 +0200

    Initial commit

--- Data-Structures/Heaps-BST-Exercise-Exercises/03.MinHeap/MinHeap.cs
+++ Data-Structures/Heaps-BST-Exercise-Exercises/03.MinHeap/MinHeap.cs
@@ -14,21 +14,109 @@ namespace _03.MinHeap
             this.elements = new List<T>();
         }
 
+        public int Size => this.elements.Count;
+
         public int Count => throw new NotImplementedException();
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this.elements.Add(element);
+            this.HeapifyUp(this.Size - 1);
         }
 
         public T ExtractMin()
         {
-            throw new NotImplementedException();
+            this.ValidateIfEmpty();
+
+            T firstElement = this.elements[0];
+
+            this.Swap(0, this.Size - 1);
+
+            this.elements.RemoveAt(this.Size - 1);
+
+            this.HeapifyDown(0);
+
+            return firstElement;
+        }
+
+        private void HeapifyDown(int index)
+        {
+            var smallerChildIndex = this.GetSmallerChildIndex(index);
+
+            while (IsIndexValid(smallerChildIndex) && this.IsGreater(index, smallerChildIndex))
+            {
+                this.Swap(smallerChildIndex, index);
+
+                index = smallerChildIndex;
+                smallerChildIndex = this.GetSmallerChildIndex(index);
+            }
+        }
+
+        protected bool IsIndexValid(int index)
+        {
+            return index >= 0 && index < this.elements.Count;
+        }
+
+        protected int GetSmallerChildIndex(int index)
+        {
+            var firstChildIndex = index * 2 + 1;
+            var secondChildIndex = index * 2 + 2;
+
+            if (secondChildIndex < this.elements.Count)
+            {
+                if (this.IsGreater(secondChildIndex, firstChildIndex))
+                {
+                    return firstChildIndex;
+                }
+
+                return secondChildIndex;
+            }
+            else if (firstChildIndex < this.elements.Count)
+            {
+                return firstChildIndex;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        protected bool IsGreater(int index, int parentIndex)
+        {
+            return this.elements[index]
+                .CompareTo(this.elements[parentIndex]) > 0;
+        }
+
+        protected void ValidateIfEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private void HeapifyUp(int index)
+        {
+            int parentIndex = (index - 1) / 2;
+            while (index > 0 && IsGreater(parentIndex, index))
+            {
+                this.Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = (index - 1) / 2;
+            }
+        }
+
+        protected void Swap(int index, int parentIndex)
+        {
+            var temp = this.elements[index];
+            this.elements[index] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.ValidateIfEmpty();
+            return this.elements[0];
         }
     }
 }
--- Data-Structures/Heaps-BST-Exercise-Exercises/03.MinHeap/PriorityQueue.cs
+++ Data-Structures/Heaps-BST-Exercise-Exercises/03.MinHeap/PriorityQueue.cs
@@ -5,24 +5,78 @@ namespace _03.MinHeap
 {
     public class PriorityQueue<T> : MinHeap<T> where T : IComparable<T>
     {
+        private Dictionary<T, int> indexesByKey;
+
         public PriorityQueue()
         {
+            this.indexesByKey = new Dictionary<T, int>();
             this.elements = new List<T>();
         }
 
+        public int Count => this.elements.Count;
+
         public void Enqueue(T element)
         {
-            throw new NotImplementedException();
+            this.elements.Add(element);
+            this.indexesByKey.Add(element, this.Count - 1);
+            this.HeapifyUp(this.Count - 1);
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            this.ValidateIfEmpty();
+
+            T firstElement = this.elements[0];
+
+            this.Swap(0, this.Count - 1);
+
+            this.elements.RemoveAt(this.Count - 1);
+            this.indexesByKey.Remove(firstElement);
+
+            this.HeapifyDown(0);
+
+            return firstElement;
+        }
+
+        private void HeapifyDown(int index)
+        {
+            var smallerChildIndex = this.GetSmallerChildIndex(index);
+
+            while (IsIndexValid(smallerChildIndex) && this.IsGreater(index, smallerChildIndex))
+            {
+                this.Swap(smallerChildIndex, index);
+
+                index = smallerChildIndex;
+                smallerChildIndex = this.GetSmallerChildIndex(index);
+            }
+        }
+
+        private void HeapifyUp(int index)
+        {
+            int parentIndex = (index - 1) / 2;
+            while (index > 0 && IsGreater(parentIndex, index))
+            {
+                this.Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = (index - 1) / 2;
+            }
+        }
+
+        private new void Swap(int index, int parentIndex)
+        {
+            var temp = this.elements[index];
+            this.elements[index] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
+
+            this.indexesByKey[this.elements[parentIndex]] = parentIndex;
+            this.indexesByKey[this.elements[index]] = index;
         }
 
         public void DecreaseKey(T key)
         {
-            throw new NotImplementedException();
+            // This solution -> ( this.elements.FindIndex(x => x.Equals(key)); ) is not optimal.
+            // By keeping a map of all indexes we achieve O(logN) complexity. 
+            this.HeapifyUp(this.indexesByKey[key]);
         }
     }
 }
