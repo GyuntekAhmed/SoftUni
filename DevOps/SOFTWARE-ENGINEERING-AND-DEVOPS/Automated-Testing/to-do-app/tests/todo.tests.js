863329e279f33b1fbba93b90c3b68e912d0eb017
gyuntekahmed <gyuntekahmed@abv.bg>
2024-01-20 23:32:26 +0200

    Add folder for Automated-Testing and To-Do app with unit tests. Initial commit

---
+++ DevOps/SOFTWARE-ENGINEERING-AND-DEVOPS/Automated-Testing/to-do-app/tests/todo.tests.js
@@ -0,0 +1,60 @@
+const { test, expect } = require("playwright/test");
+
+// Verify if a user can add a task
+test("user can add a task", async ({ page }) => {
+  await page.goto("http://localhost:5500/");
+  await page.fill("#task-input", "Test Task");
+  await page.click("#add-task");
+
+  const taskText = await page.textContent(".task");
+
+  expect(taskText).toContain("Test Task");
+});
+
+// Verify if a user can delete a task
+test("user can delete a task", async ({ page }) => {
+  // Add a task
+  await page.goto("http://localhost:5500/");
+  await page.fill("#task-input", "Test Task");
+  await page.click("#add-task");
+  // Delete the task
+  await page.click(".task .delete-task");
+
+  const tasks = await page.$$eval(".task", (tasks) =>
+    tasks.map((task) => task.textContent)
+  );
+  expect(tasks).not.toContain("Test Task");
+});
+
+// Verify if a user can mark a task as complete
+test("user can mark a task as complete", async ({ page }) => {
+  // Add a task
+  await page.goto("http://localhost:5500/");
+  await page.fill("#task-input", "Test Task");
+  await page.click("#add-task");
+
+  // Mark the task as complete
+  await page.click(".task .task-complete");
+
+  const completedTask = await page.$(".task-completed");
+
+  expect(completedTask).not.toBeNull();
+});
+
+// Verify if a user can filter
+test("user can filter", async ({ page }) => {
+  // Add a task
+  await page.goto("http://localhost:5500/");
+  await page.fill("#task-input", "Test Task");
+  await page.click("#add-task");
+
+  // Mark the task as complete
+  await page.click(".task .task-complete");
+
+  // Filter tasks
+  await page.selectOption("#filter", "Completed");
+
+  const incompleteTask = await page.$(".task:not(.completed)");
+
+  expect(incompleteTask).toBeNull();
+});
