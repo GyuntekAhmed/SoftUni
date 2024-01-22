4ef3b8ace9ac6e01d5f72e6b6852711ceba3a64e
gyuntekahmed <gyuntekahmed@abv.bg>
2024-01-22 20:26:40 +0200

    Add more tests

--- DevOps/SOFTWARE-ENGINEERING-AND-DEVOPS/Books/api.test.js
+++ DevOps/SOFTWARE-ENGINEERING-AND-DEVOPS/Books/api.test.js
@@ -14,12 +14,13 @@ describe("Books API Tests", () => {
   };
 
   it("should POST a book", (done) => {
-    chai.request(server)
-    .post("/books")
-    .send(requestBody)
-    .end((err, resp) => {
+    chai
+      .request(server)
+      .post("/books")
+      .send(requestBody)
+      .end((err, resp) => {
         if (err) {
-            return done(err)
+          return done(err);
         }
         expect(resp.statusCode).to.be.equal(201);
         expect(resp.body).to.be.a("object");
@@ -27,6 +28,60 @@ describe("Books API Tests", () => {
         expect(resp.body.title).to.be.equal(requestBody.title);
         expect(resp.body.author).to.be.equal(requestBody.author);
         done();
-    })
+      });
+  });
+
+  it("should GET all books", (done) => {
+    chai
+      .request(server)
+      .get("/books")
+      .end((err, resp) => {
+        if (err) {
+          return done(err);
+        }
+        expect(resp.statusCode, "Status code: ").to.equal(200);
+        expect(resp.body).to.be.a("array");
+        done();
+      });
+  });
+
+  it("should GET a single book", (done) => {
+    chai
+      .request(server)
+      .get("/books/1")
+      .end((err, resp) => {
+        if (err) {
+          return done(err);
+        }
+        expect(resp.statusCode, "Status code: ").to.equal(200);
+        expect(resp.body).to.be.a("object");
+        expect(resp.body.id).to.exist;
+        expect(resp.body.author).to.exist;
+        expect(resp.body.title).to.exist;
+        done();
+      });
+  });
+
+  it("should UPDATE a book", (done) => {
+    const reqBody = {
+      id: "1",
+      title: "DevOps Magic 2 Updated",
+      author: "Gyuni",
+    };
+    chai
+      .request(server)
+      .put(`/books/${reqBody.id}`)
+      .send(requestBody)
+      .end((err, resp) => {
+        if (err) {
+          return done(err);
+        }
+        expect(resp.statusCode).to.be.equal(200);
+        expect(resp.body).to.be.a("object");
+        expect(resp.body.id).to.be.equal(requestBody.id);
+        expect(resp.body.title).to.be.equal(requestBody.title);
+        expect(resp.body.author).to.be.equal(requestBody.author);
+        done();
+      });
   });
 });
