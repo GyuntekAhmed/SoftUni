72232d4fc261b237bdb62c4541a7bc013ba301ae
gyuntekahmed <gyuntekahmed@abv.bg>
2024-01-27 23:24:41 +0200

    Repo init

---
+++ DevOps/SOFTWARE-ENGINEERING-AND-DEVOPS/Workshop/backend/models/todos/todo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Todo = mongoose.model('Todo', {
+    text : {
+        type: String,
+        trim: true,
+        required: true
+    }
+});
+
+module.exports = {Todo};
