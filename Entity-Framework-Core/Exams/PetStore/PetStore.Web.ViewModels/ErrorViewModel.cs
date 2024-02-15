24f2145f9b929a41887ebb5413fa67bcf8b8329b
gyuntekahmed <gyuntekahmed@abv.bg>
2024-02-15 19:52:53 +0200

    Add PetStore project and initial commit

---
+++ Entity-Framework-Core/Exams/PetStore/PetStore.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PetStore.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; } = null!;
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Entity-Framework-Core/Exams/PetStore/PetStore.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿namespace PetStore.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    using ViewModels;
+
+    public class HomeController : Controller
+    {
+        public HomeController(ILogger<HomeController> logger)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
